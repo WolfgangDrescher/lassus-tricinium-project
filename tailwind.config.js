b9ccf67664b920f158ff9e5eaa5cfc96d23a7e8e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-21 16:21:50 +0100

    Add container tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                md: '3rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
+        },
     },
     plugins: [],
 };
