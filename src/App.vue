80caba3c861bd41c54fb94bdeb9a5573e2220d67
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-21 15:45:19 +0100

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
 <template>
+    <header>
+        <div class="wrapper">
+            <nav>
+                <RouterLink :to="{name: 'index'}">Home</RouterLink>
+                <RouterLink :to="{name: 'about'}">About</RouterLink>
+                <RouterLink :to="{name: 'tricinium_list'}">tricinium</RouterLink>
+            </nav>
+        </div>
+    </header>
     <main>
         <RouterView />
     </main>
