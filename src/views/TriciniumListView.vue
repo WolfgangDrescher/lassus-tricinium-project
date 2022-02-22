8b24b56d58ff69a0cf1028786b076ebbd7601e81
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-23 00:19:34 +0100

    Add TriciniumListView component

--- src/views/TriciniumListView.vue
+++ src/views/TriciniumListView.vue
@@ -1,7 +1,29 @@
 <script setup>
+import { reactive } from 'vue';
 import Heading from '../components/Heading.vue';
+import meta from 'lassus-geistliche-psalmen/meta.json';
+import TriciniumListItem from '../components/TriciniumListItem.vue';
+import TriciniumFilter from '../components/TriciniumFilter.vue';
+import { useFilter } from '../composables/useFilter.js';
+
+const filter = reactive({
+    composer: '',
+    title: '',
+    nr: '',
+});
+
+const { filteredElements } = useFilter(meta, filter);
+
 </script>
 
 <template>
     <Heading>Tricinium Liste</Heading>
+    
+    <TriciniumFilter :filter="filter" />
+
+    <div class="grid grid-cols-4 gap-4">
+        <div v-for="tricinium in filteredElements" :key="tricinium.id">
+            <TriciniumListItem :tricinium="tricinium" />
+        </div>
+    </div>
 </template>
