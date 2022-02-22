c20004e959bb1508b6e7ba3d05a7fd12f132f139
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-22 23:51:55 +0100

    Fix container component

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,16 +1,14 @@
 <script setup>
 defineProps({
-    props: {
-        fluid: {
-            type: Boolean,
-            default: false,
-        },
+    fluid: {
+        type: Boolean,
+        default: false,
     },
 });
 </script>
 
 <template>
     <div :class="{ 'mx-auto px-2 md:px-4': fluid, container: !fluid }">
-        <slot />
+        <slot></slot>
     </div>
 </template>
