421015b5d9f8251dbd096e13d7d5dbfeaa300fdb
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-22 23:58:13 +0100

    Add TriciniumListItem component

---
+++ src/components/TriciniumListItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    tricinium: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <RouterLink :to="{name: 'tricinium', params: {id: tricinium.id}}">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ tricinium.title }}</h5>
+        </RouterLink>
+        <pre v-text="tricinium"></pre>
+        <RouterLink :to="{name: 'tricinium', params: {id: tricinium.id}}" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </RouterLink>
+    </div>
+</template>
