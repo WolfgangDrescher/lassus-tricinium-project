b289787d48596cc67ea99a80587c0b7dc0cf376f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-22 23:14:36 +0100

    Fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     content: [
         './src/components/**/*.{vue,html,js}',
-        './src/view/**/*.{vue,html,js}',
+        './src/views/**/*.{vue,html,js}',
     ],
     theme: {
         extend: {},
