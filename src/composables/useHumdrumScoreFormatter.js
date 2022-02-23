7a47286d2afd7fccb2477a306a494f1058e444f6
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-23 23:21:30 +0100

    Add useHumdrumScoreFormatter composable

---
+++ src/composables/useHumdrumScoreFormatter.js
@@ -0,0 +1,38 @@
+import { ref, computed, readonly } from 'vue';
+
+export function useHumdrumScoreFormatter(data, options) {
+    const filterPrefix = '!!!filter: ';
+
+    const showFilterPrefix = false;
+
+    const filters = ref([
+        'cint -O --search "3 2 3"',
+        'cint -O --search "3 -2 3"',
+    ]);
+
+    const filtersAsString = computed({
+        get() {
+            return filters.value.map((f) => (showFilterPrefix ? `${filterPrefix}${f}` : f)).join('\n');
+        },
+        set(value) {
+            filters.value = value.split('\n').map((f) => {
+                line = showFilterPrefix ? line.replace(new RegExp(`/${filterPrefix}/`), '') : line;
+                return line.trim();
+            });
+        },
+    });
+
+    const prefixedFiltersAsString = computed(() => {
+        return filters.value.map((f) => `${filterPrefix}${f}`).join('\n'); 
+    });
+
+    const formattedScoreData = computed(() => {
+        return `${prefixedFiltersAsString.value}\n${data.value}`.replace(/^\s+|\s+$/g, '');
+    });
+
+    return {
+        filters: readonly(filters),
+        filtersAsString,
+        formattedScoreData: readonly(formattedScoreData),
+    }
+};
