a01b92aa1e7bd43d508c2c418b14c57298d1afe9
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-02-23 23:23:21 +0100

    Add HumdrumScoreAnalyzer component

---
+++ src/components/HumdrumScoreAnalyzer.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed, ref } from 'vue';
+import VerovioCanvas from 'vue-verovio-canvas';
+import { useHumdrumScoreFormatter } from '../composables/useHumdrumScoreFormatter';
+
+const props = defineProps({
+    url: {
+        type: String,
+        required: true,
+    },
+})
+
+const response = await fetch(props.url);
+if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`);
+}
+const data = ref(await response.text());
+
+const { formattedScoreData, filtersAsString, } = useHumdrumScoreFormatter(data, {});
+
+const verovioOptions = computed(() => {
+    return {
+        spacingSystem: 24,
+        data: formattedScoreData.value,
+    };
+});
+
+</script>
+
+<template>
+    <textarea
+        rows="10"
+        v-model.lazy="filtersAsString"
+        class="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+    ></textarea>
+    <VerovioCanvas v-bind="verovioOptions" />
+</template>
