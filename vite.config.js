c65100a03aeb0e9e8f6fdbef64f484d22bbc78d0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-11 09:52:17 +0100

    Setup vite-plugin-ssr

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import ssr from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), ssr()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
