3ee6f5f623f690ea0b8d0b3e3f6ddcb4a9e5a037
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-11 12:23:24 +0100

    Add favicon

--- src/renderer/_default.page.server.js
+++ src/renderer/_default.page.server.js
@@ -19,6 +19,7 @@ async function render(pageContext) {
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
