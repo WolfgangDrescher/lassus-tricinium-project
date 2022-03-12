236472a5820dae59a289fa04282e36be00086d14
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 19:45:51 +0100

    Improve TriciniumListItem styles

--- src/components/TriciniumListItem.vue
+++ src/components/TriciniumListItem.vue
@@ -1,5 +1,8 @@
 <script setup>
 import Link from './Link.vue';
+import BadgeCloud from './BadgeCloud.vue';
+import Badge from './Badge.vue';
+
 defineProps({
     tricinium: {
         type: Object,
@@ -9,25 +12,40 @@ defineProps({
 </script>
 
 <template>
-    <div
-        class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-    >
-        <Link :href="`/tricinium/${tricinium.id}`">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ `${tricinium.nr}. ${tricinium.title}` }}</h5>
-        </Link>
-        <pre v-text="tricinium"></pre>
-        <Link
-            :href="`/tricinium/${tricinium.id}`"
-            class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-            Read more
-            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                ></path>
-            </svg>
-        </Link>
+    <div class="p-6 shadow border border-gray-100 rounded">
+        <div class="flex items-center border-b border-gray-200 pb-6">
+            <div class="w-12 h-12 flex justify-center items-center font-serif text-4xl">
+                {{ tricinium.nr }}.
+            </div>
+            <div class="flex items-start justify-between w-full">
+                <div class="pl-3 w-full">
+                    <div class="text-xl font-medium leading-5 text-gray-800">
+                        <Link :href="`/tricinium/${tricinium.id}`">
+                            {{ tricinium.title }}
+                        </Link>
+                    </div>
+                    <div class="text-sm leading-normal pt-1 text-gray-500">
+                        {{ tricinium.composer }}
+                    </div>
+                </div>
+                <div>
+                    <a :href="tricinium.vhvHref" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="px-2">
+            <div v-if="tricinium.hasLyrics" class="text-sm leading-5 py-4 text-gray-600">
+                {{ tricinium.lyricsAsString() }}
+            </div>
+            <BadgeCloud>
+                <Badge text-size="sm" v-if="tricinium.transposed">Transponiert</Badge>
+                <Badge text-size="sm" v-if="tricinium.finalis">{{ tricinium.finalis }}</Badge>
+                <Badge text-size="sm" v-if="tricinium.mode">{{ tricinium.mode }}</Badge>
+            </BadgeCloud>
+        </div>
     </div>
 </template>
