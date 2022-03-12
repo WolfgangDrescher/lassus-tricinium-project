95c914386f3dbb4f3f5f142c065d1a164d49daea
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 18:06:19 +0100

    Pass tricinium to page

--- src/pages/tricinium/tricinium.page.server.js
+++ src/pages/tricinium/tricinium.page.server.js
@@ -1,7 +1,11 @@
+import tricinia from 'lassus-geistliche-psalmen';
 
 export async function onBeforeRender(pageContext) {
     const { name } = pageContext.routeParams;
-    const pageProps = { name };
+    const tricinium = tricinia.find((t) => t.id === name);
+    if (!tricinium) throw new Error(`Tricinium "${name}" not found`);
+    const pageProps = { tricinium };
+
     return {
         pageContext: {
             pageProps,
