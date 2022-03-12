765722f6050a0b7ee500de6adc27726eb38181d9
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 18:02:05 +0100

    Remove max width

--- src/components/TriciniumListItem.vue
+++ src/components/TriciniumListItem.vue
@@ -10,7 +10,7 @@ defineProps({
 
 <template>
     <div
-        class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+        class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
     >
         <Link :href="`/tricinium/${tricinium.id}`">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ tricinium.title }}</h5>
