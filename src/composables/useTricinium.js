378ec50a4284ffe93020bf6739aedd8480a7aa13
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 19:46:06 +0100

    Add useTricinium composable

---
+++ src/composables/useTricinium.js
@@ -0,0 +1,72 @@
+import { computed } from 'vue';
+
+class Tricinium {
+    constructor(tricinium) {
+        this.tricinium = tricinium;
+    }
+
+    get id() {
+        return this.tricinium.id;
+    }
+
+    get nr() {
+        return this.tricinium.nr;
+    }
+
+    get title() {
+        return this.tricinium.title;
+    }
+
+    get incipit() {
+        return this.tricinium.incipit;
+    }
+
+    get composer() {
+        return this.tricinium.composer;
+    }
+
+    get rawFile() {
+        return this.tricinium.rawFile;
+    }
+
+    get lyrics() {
+        return this.tricinium.lyrics;
+    }
+
+    get mode() {
+        return this.tricinium.mode;
+    }
+
+    get transposed() {
+        return this.tricinium.transposed;
+    }
+
+    get finalis() {
+        return this.tricinium.finalis;
+    }
+
+    get clefs() {
+        return this.tricinium.clefs;
+    }
+
+    get hasLyrics() {
+        return !!this.lyrics?.length;
+    }
+
+    lyricsAsString(trimSlash = true, separator = ', ') {
+        return this.lyrics?.map((l) => (trimSlash ? l.text.replace(/\/$/g, '') : l.text)).join(separator) || null;
+    }
+
+    get vhvHref() {
+        return `https://verovio.humdrum.org/?file=${this.rawFile}`;
+    }
+}
+
+export function useTricinium(elements) {
+    if (Array.isArray(elements)) {
+        return elements.map((t) => new Tricinium(t))
+    } else if(elements === 'object' && elements !== null) {
+        return new Tricinium(elements);
+    }
+    throw new Error('Cannot convert passsed argument to Tricinium object');
+}
