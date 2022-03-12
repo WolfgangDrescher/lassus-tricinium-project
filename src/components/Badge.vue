ac385d55196e2e03b490e9c35808d9a72ca5e73e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 19:45:36 +0100

    Add Badge component

---
+++ src/components/Badge.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+    textSize: {
+        default: 'base',
+    },
+});
+// text-xs text-sm text-base text-lg text-xl
+const { text } = toRefs(props);
+</script>
+
+<template>
+    <div :class="`py-2 px-4 text-${textSize} leading-3 text-indigo-700 rounded-full bg-indigo-100 border border-indigo-200`">
+        <slot></slot>
+    </div>
+</template>
