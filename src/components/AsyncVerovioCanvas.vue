1e01bbde9cdee8994849831cb4f9791ea41b12fa
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-12 13:51:17 +0100

    Add AsyncVerovioCanvas component

---
+++ src/components/AsyncVerovioCanvas.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+// import 'vue-verovio-canvas/dist/style.css';
+const AsyncVerovioCanvas = defineAsyncComponent(() => import('vue-verovio-canvas'));
+</script>
+
+<template>
+    <component :is="AsyncVerovioCanvas" v-bind="{...$attrs, ...$props}"></component>
+</template>
