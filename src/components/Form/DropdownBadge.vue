2a7777287230cadd7c38ddafaa2c1563a48208cc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-13 23:25:00 +0100

    Smaller close button

--- src/components/Form/DropdownBadge.vue
+++ src/components/Form/DropdownBadge.vue
@@ -21,7 +21,7 @@ const removeOption = () => {
         <div class="text-xs font-normal leading-none max-w-full flex-initial">{{ props.label || props.value }}</div>
         <div @click="removeOption" class="cursor-pointer flex flex-auto flex-row-reverse">
             <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
             </div>
