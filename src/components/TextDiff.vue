622941782329470aee87ed6237f8d3f2e1256e9a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-13 16:41:06 +0100

    Add TextDiff component

---
+++ src/components/TextDiff.vue
@@ -0,0 +1,29 @@
+<script setup>
+import * as Diff2html from 'diff2html';
+import 'diff2html/bundles/css/diff2html.min.css';
+import { computed } from 'vue';
+
+const defaultOptions = {
+    drawFileList: false,
+    outputFormat: 'side-by-side',
+}
+
+const props = defineProps({
+    diff: {
+        type: String,
+        required: true,
+    },
+    options: {
+        type: Object,
+        default: () => ({}),
+    }
+});
+
+const diffHtml = computed(() => {
+    return Diff2html.html(Diff2html.parse(props.diff), Object.assign({}, defaultOptions, props.options));
+});
+</script>
+
+<template>
+    <div v-html="diffHtml"></div>
+</template>
