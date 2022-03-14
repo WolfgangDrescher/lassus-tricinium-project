261f9f244e8d41f8d6842f5e17b4f05157c73b14
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-14 16:36:11 +0100

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="mx-2 my-2 bg-primary-700 transition duration-150 ease-in-out hover:bg-primary-400 rounded text-white hover:text-black px-6 py-2 text-xs">
+        <slot></slot>
+    </button>
+</template>
