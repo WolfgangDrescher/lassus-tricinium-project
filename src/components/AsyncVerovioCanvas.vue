603ba0787a8f4ca3386ebfddddbc9d87742c0d40
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-14 16:45:37 +0100

    Remove async verovio component

--- src/components/AsyncVerovioCanvas.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { defineAsyncComponent } from 'vue';
-// import 'vue-verovio-canvas/dist/style.css';
-const AsyncVerovioCanvas = defineAsyncComponent(() => import('vue-verovio-canvas'));
-</script>
-
-<template>
-    <component :is="AsyncVerovioCanvas" v-bind="{...$attrs, ...$props}"></component>
-</template>
