f2312303a5837cb36196aa9cd6e40cc3e174c824
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-16 01:30:52 +0100

    Add Line class

--- src/classes/HumdrumFilters.js
+++ src/classes/HumdrumFilters.js
@@ -1,8 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export class HumdrumFilter {
-    static linePrefix = '!!!filter: ';
+class Line {
+    constructor(value, linePrefix) {
+        this.value = value || '';
+        this.prefix = linePrefix || '!!!filter: ';
+    }
 
+    toString() {
+        return `${this.prefix}${this.value}`;
+    }
+}
+
+export class HumdrumFilter {
     id = null;
     className = null;
     unique = false;
@@ -15,36 +24,39 @@ export class HumdrumFilter {
         this.className = this.constructor.name;
     }
 
+    addLine(value, prefix) {
+        this.lines.push(new Line(value, prefix));
+    }
     toString() {
-        return this.lines.map((l) => `${HumdrumFilter.linePrefix}${l}`).join('\n');
+        return this.lines.map(l => l.toString()).join('\n');
     }
 }
 
 export class HumdrumClefFilter extends HumdrumFilter {
     lines = [
-        'shed -e "s/^clefC[12]/clefG2/I; s/^clefC[34]/clefGv2/I; s/^clefC5/clefF4/I; s/^clefF[35]/clefF4/I; s/^clefG[13]/clefG2/I"',
+        new Line('shed -e "s/^clefC[12]/clefG2/I; s/^clefC[34]/clefGv2/I; s/^clefC5/clefF4/I; s/^clefF[35]/clefF4/I; s/^clefG[13]/clefG2/I"'),
     ];
 }
 
 export class HumdrumMensuralFilter extends HumdrumFilter {
-    lines = ['kern2mens'];
     unique = true;
+    lines = [new Line('kern2mens')];
 }
 
 export class HumdrumLyricsFilter extends HumdrumFilter {
     unique = true;
     priority = -1;
-    lines = ['extract -I **text'];
+    lines = [new Line('extract -I **text')];
 }
 
 export class HumdrumEditorialAccidentalsFilter extends HumdrumFilter {
-    lines = ["shed -ke 's/i/y/g'"];
     unique = true;
+    lines = [new Line("shed -ke 's/i/y/g'")];
 }
 
 export class HumdrumCompositeRhythmFilter extends HumdrumFilter {
-    lines = ['composite'];
     unique = true;
+    lines = [new Line('composite')];
 }
 
 export class HumdrumMeasureFilter extends HumdrumFilter {
@@ -54,7 +66,7 @@ export class HumdrumMeasureFilter extends HumdrumFilter {
         if (!this.validateValue(value)) {
             throw new Error(`Cannot set "${value}" as value for ${this.className}`);
         }
-        this.lines.push(`myank -m ${value}`);
+        this.addLine(`myank -m ${value}`);
     }
 
     validateValue(value, numberOfMeasures) {
@@ -72,7 +84,7 @@ export class HumdrumExtractFilter extends HumdrumFilter {
         if (!this.validateValue(value)) {
             throw new Error(`Cannot set "${value}" as value for ${this.className}`);
         }
-        this.lines.push(`extract -f ${value}`);
+        this.addLine(`extract -f ${value}`);
     }
 
     validateValue(value) {
