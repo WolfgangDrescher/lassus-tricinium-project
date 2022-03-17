4b938b7cd40723e568e2ece2f9f7193977f74fea
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-18 00:18:06 +0100

    Add statistic pages

---
+++ src/pages/statistics/clefs.page.vue
@@ -0,0 +1,13 @@
+<script setup>
+import StatisticsIndexPage from './index.page.vue';
+import Heading from '../../components/Heading.vue';
+
+const props = defineProps(['tricinia']);
+</script>
+
+<template>
+    <StatisticsIndexPage>
+        <Heading>Clef statistics</Heading>
+        <pre v-text="tricinia"></pre>
+    </StatisticsIndexPage>
+</template>
