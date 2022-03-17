dff52a1e8c29a0da503a9503267116559194eeff
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-18 00:18:31 +0100

    Add StatisticsSidebarItem component

---
+++ src/components/StatisticsSidebarItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { usePageContext } from '../composables/usePageContext';
+import Link from './Link.vue';
+const pageContext = usePageContext();
+</script>
+
+<template>
+    <li>
+        <Link
+            v-bind="{...$attrs}"
+            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100"
+            :class="{ active: pageContext.urlPathname === $attrs.href }"
+        >
+            <span class="flex-1 ml-3 whitespace-nowrap">
+                <slot></slot>
+            </span>
+        </Link>
+    </li>
+</template>
