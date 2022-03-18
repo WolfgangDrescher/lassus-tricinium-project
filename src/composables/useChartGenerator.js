dc894ce50be847fd130d7ddc9e4faf704eff5364
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-18 22:30:10 +0100

    Add useChartGenerator composable

---
+++ src/composables/useChartGenerator.js
@@ -0,0 +1,69 @@
+import { computed, ref, readonly } from 'vue';
+
+export function useChartGenerator(elements, filterValue) {
+    const dimension = ref('composer');
+
+    const elementsGroupedByDimension = computed(() => {
+        return elements.value.reduce((previousValue, tricinium) => {
+            let index = previousValue.findIndex((d) => d.label === (dimension.value ? tricinium[dimension.value] : 'All'));
+            if (index === -1) {
+                index = -1 + previousValue.push({
+                    label: dimension.value ? tricinium[dimension.value] : 'All',
+                    data: [],
+                });
+            }
+            previousValue[index].data.push(tricinium);
+            return previousValue;
+        }, []);
+    });
+
+    const datasets = computed(() => {
+        return elementsGroupedByDimension.value.map((dataset) => {
+            return {
+                ...dataset,
+                data: dataset.data.reduce((previousValue, tricinium) => {
+                    const x = filterValue(tricinium);
+                    let index = previousValue.findIndex((d) => d.x === x);
+                    if (index === -1) {
+                        index =
+                            -1 +
+                            previousValue.push({
+                                x,
+                                y: 0,
+                            });
+                    }
+                    previousValue[index].y++;
+                    return previousValue;
+                }, []),
+            };
+        });
+    });
+
+    const config = computed(() => ({
+        type: 'bar',
+        data: {
+            datasets: datasets.value,
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: !!dimension.value,
+                },
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        precision: 0,
+                    },
+                },
+            },
+        },
+    }));
+
+    return {
+        dimension,
+        datasets,
+        config,
+    };
+}
