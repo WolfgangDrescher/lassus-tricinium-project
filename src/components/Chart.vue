7c6bb9b9b8e65b3b778a152ae3b410d7430cc5c0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-18 12:40:20 +0100

    Add Chart component

---
+++ src/components/Chart.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import Chart from 'chart.js/auto';
+
+const props = defineProps(['options'])
+
+const chartRef = ref(null);
+
+onMounted(() => {
+    new Chart(chartRef.value, props.options);
+});
+</script>
+
+<template>
+    <div class="aspect-video">
+        <canvas ref="chartRef"></canvas>
+    </div>
+</template>
