b75cf699a20b0125964e1246851fcf7d92e50d03
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-19 22:37:38 +0100

    Add form label

--- src/components/Form/Dropdown.vue
+++ src/components/Form/Dropdown.vue
@@ -2,6 +2,7 @@
 import { computed, ref, watch } from 'vue';
 import DropdownBadge from './DropdownBadge.vue';
 import DropdownOption from './DropdownOption.vue';
+import FormLabel from './FormLabel.vue';
 import { vOnClickOutside } from '@vueuse/components';
 
 const emit = defineEmits(['update:modelValue']);
@@ -92,39 +93,41 @@ const filteredOptions = computed(() => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center">
-        <div class="w-full">
-            <div class="flex flex-col items-center relative">
-                <div class="w-full">
-                    <div class="p-1 flex border border-gray-200 bg-white rounded">
-                        <div class="flex flex-auto flex-wrap">
-                            <div v-if="!props.modelValue?.length && props.emptyValuePlaceholder" class="p-1 px-2 text-gray-500" v-text="props.emptyValuePlaceholder"></div>
-                            <template v-if="props.multiple">
-                                <DropdownBadge v-for="(value, index) in props.modelValue" :key="index" :text="getOptionTextFromValue(value)" :value="value" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
-                            </template>
-                            <DropdownBadge v-else-if="props.modelValue" :text="getOptionTextFromValue(props.modelValue)" :value="props.modelValue" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
-                            <div v-if="props.searchEnabled" class="flex-1">
-                                <input v-model="searchString" placeholder="" class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800">
+    <FormLabel>
+        <div class="flex flex-col items-center">
+            <div class="w-full">
+                <div class="flex flex-col items-center relative">
+                    <div class="w-full">
+                        <div class="p-1 flex border border-gray-200 bg-white rounded">
+                            <div class="flex flex-auto flex-wrap">
+                                <div v-if="!props.modelValue?.length && props.emptyValuePlaceholder" class="p-1 px-2 text-gray-500" v-text="props.emptyValuePlaceholder"></div>
+                                <template v-if="props.multiple">
+                                    <DropdownBadge v-for="(value, index) in props.modelValue" :key="index" :text="getOptionTextFromValue(value)" :value="value" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
+                                </template>
+                                <DropdownBadge v-else-if="props.modelValue" :text="getOptionTextFromValue(props.modelValue)" :value="props.modelValue" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
+                                <div v-if="props.searchEnabled" class="flex-1">
+                                    <input v-model="searchString" placeholder="" class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800">
+                                </div>
+                            </div>
+                            <div class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
+                                <button @click="toggleDropdown" class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
+                                    <svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+                                    </svg>
+                                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
-                        <div class="text-gray-300 w-8 py-1 pl-2 pr-1 border-l flex items-center border-gray-200">
-                            <button @click="toggleDropdown" class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                                <svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
-                                </svg>
-                                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                                </svg>
-                            </button>
-                        </div>
                     </div>
-                </div>
-                <div v-if="isOpen" v-on-click-outside="closeDropdown" class="absolute shadow border border-gray-200 mt-2 top-full bg-white z-40 w-full lef-0 rounded max-h-80 overflow-y-auto">
-                    <div class="flex flex-col w-full">
-                        <DropdownOption v-for="(option, index) in filteredOptions" :key="index" :value="option.value" :text="option.text" :selected="optionIsSelected(option.value)" @toggleOption="toggleOption" />
+                    <div v-if="isOpen" v-on-click-outside="closeDropdown" class="absolute shadow border border-gray-200 mt-2 top-full bg-white z-40 w-full lef-0 rounded max-h-80 overflow-y-auto">
+                        <div class="flex flex-col w-full">
+                            <DropdownOption v-for="(option, index) in filteredOptions" :key="index" :value="option.value" :text="option.text" :selected="optionIsSelected(option.value)" @toggleOption="toggleOption" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>  
+        </div>  
+    </FormLabel>
 </template>
