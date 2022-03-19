89cf1343367a740ed2e03c7bda8223cb57f57e99
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-19 01:33:20 +0100

    Display data table

--- src/pages/statistics/clefs.page.vue
+++ src/pages/statistics/clefs.page.vue
@@ -3,16 +3,19 @@ import StatisticsIndexPage from './index.page.vue';
 import Heading from '../../components/Heading.vue';
 import Chart from '../../components/Chart.vue';
 import TriciniumFilter from '../../components/TriciniumFilter.vue';
+import DataTable from '../../components/DataTable.vue';
 import { useTricinium } from '../../composables/useTricinium.js';
 import { useTriciniumFilter } from '../../composables/useTriciniumFilter.js';
 import { useChartGenerator } from '../../composables/useChartGenerator.js';
+import { useDatasetTransformer } from '../../composables/useDatasetTransformer.js';
 
 const props = defineProps(['tricinia']);
 
 const tricinia = useTricinium(props.tricinia);
 
 const { filteredElements, filter } = useTriciniumFilter(tricinia);
-const { config, dimension } = useChartGenerator(filteredElements, (tricinium) => tricinium.mode);
+const { datasets, config, dimension } = useChartGenerator(filteredElements, (tricinium) => tricinium.mode);
+const { headers, items } = useDatasetTransformer(datasets);
 </script>
 
 <template>
@@ -33,5 +36,6 @@ const { config, dimension } = useChartGenerator(filteredElements, (tricinium) =>
             <option value="finalis">finalis</option>
         </select>
         <Chart :config="config" />
+        <DataTable :headers="headers" :items="items"></DataTable>
     </StatisticsIndexPage>
 </template>
