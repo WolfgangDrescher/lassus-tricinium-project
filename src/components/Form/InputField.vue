ee1e121de4ae09339b759224cb4af294cb913546
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-19 22:36:25 +0100

    Add input field component

---
+++ src/components/Form/InputField.vue
@@ -0,0 +1,15 @@
+<script setup>
+import FormLabel from './FormLabel.vue';
+const props = defineProps(['label', 'modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+function onChange(event) {
+    emit('update:modelValue', event.target.value);
+}
+</script>
+
+<template>
+    <FormLabel :label="props.label">
+        <input :value="modelValue" @input="onChange" v-bind="$attrs" class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" />
+    </FormLabel>
+</template>
