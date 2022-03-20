010fef0ac8dbcdcfde3a55ff59a59b5a2dcf779f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-20 12:46:35 +0100

    Remove static filter buttons

--- src/components/InteractiveHumdrumScoreFilterGroup.vue
+++ src/components/InteractiveHumdrumScoreFilterGroup.vue
@@ -28,15 +28,6 @@ defineProps({
 })
 const emit = defineEmits(['addFilter', 'removeFilter']);
 
-const measureFilterValue = ref(null);
-const extractFilterValue = ref(null);
-const transposeValue = ref('');
-const transposeMode = ref('-t');
-const cintFilterInterval = ref(6);
-const cintFilterDirection = ref(2);
-const cintFilterColor = ref('#f97316');
-const shedValue = ref(null);
-
 const selectedFilter = ref(null);
 
 const allFilters = [
@@ -89,31 +80,7 @@ function applyFilter(args) {
         <Dropdown v-model="selectedFilter" :options="filterOptions" />
         <HumdrumFilterConfigurator :filter="selectedFilter" @applyFilter="applyFilter"></HumdrumFilterConfigurator>
     </div>
-    <div class="my-4 w-full border-t-4 border-gray-300"></div>
-    <Button @click="addFilter(new ClefFilter())">Modern clefs</Button>
-    <Button @click="addFilter(new MensuralFilter())">Mensural</Button>
-    <Button @click="addFilter(new LyricsFilter())">Remove lyrics</Button>
-    <Button @click="addFilter(new EditorialAccidentalsFilter())">Remove editorial accidentals</Button>
-    <Button @click="addFilter(new CompositeRhythmFilter())">Composite rhythm</Button>
-    <Button @click="addFilter(new DissonantFilter())">Dissonant</Button>
-    <Button @click="addFilter(new AutobeamFilter())">Autobeam</Button>
-    <Button @click="addFilter(new ImitationFilter())">Imitation</Button>
-    <Button @click="addFilter(new MelismaFilter())">Melisma</Button>
-    <Button @click="addFilter(new SicFilter())">Sic</Button>
-    <input v-model="transposeMode" />
-    <input v-model="transposeValue" />
-    <Button @click="addFilter(new TransposeFilter(transposeMode, transposeValue))">Transpose</Button>
-    <input v-model="measureFilterValue" />
-    <Button @click="addFilter(new MeasureFilter(measureFilterValue))">Measure</Button>
-    <input v-model="extractFilterValue" />
-    <Button @click="addFilter(new ExtractFilter(extractFilterValue))">Extract</Button>
-    <input v-model="cintFilterInterval" />
-    <input v-model="cintFilterDirection" />
-    <input v-model="cintFilterColor" />
-    <Button @click="addFilter(new ParallelIntervalsFilter(cintFilterInterval, cintFilterDirection, cintFilterColor))">Parallel intervals</Button>
-    <input v-model="shedValue" />
-    <Button @click="addFilter(new ShedFilter(shedValue))">Shed</Button>
-    <div class="my-4 w-full border-t-4 border-gray-300"></div>
+
     <BadgeGroup>
         <Badge v-for="filter in filters" :key="filter.id">
             {{ filter.className }}
