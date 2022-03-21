cc2dc4bf2e1077fdc38d51e9776f8b71b127d6e0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-21 21:45:08 +0100

    Improve prop access

--- src/components/Form/InputField.vue
+++ src/components/Form/InputField.vue
@@ -10,6 +10,6 @@ function onChange(event) {
 
 <template>
     <FormLabel :label="props.label">
-        <input :value="modelValue" @input="onChange" v-bind="$attrs" class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" />
+        <input :value="props.modelValue" @input="onChange" class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 bg-white font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow" />
     </FormLabel>
 </template>
