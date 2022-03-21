7aca9a6620d0e843ae3981a8cb66d46d69a0eda1
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-21 21:45:38 +0100

    Add reset method

--- src/stores/filter.js
+++ src/stores/filter.js
@@ -1,6 +1,17 @@
 import { defineStore } from 'pinia';
 import { useLocalStorage } from '@vueuse/core';
 
+const defaultFilter = {
+    composer: null,
+    title: null,
+    lyrics: null,
+    nr: null,
+    mode: null,
+    transposed: null,
+    finalis: null,
+    clefs: null,
+    searchText: null,
+};
 
 export const useFilterStore = defineStore('filter', {
     state: () => useLocalStorage('tricinium_filter', {
@@ -17,6 +28,9 @@ export const useFilterStore = defineStore('filter', {
     actions: {
         update(prop, value) {
             this[prop] = value;
+        },
+        reset() {
+            this.$patch({...defaultFilter});
         },
     },
 });
