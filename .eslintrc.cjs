968c600a61cc4876ce9a80f06dddb9f2cbcc9ea4
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-21 17:11:54 +0100

    Remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
     root: true,
     ignorePatterns: ['*.config.js'],
-    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
     env: {
         'vue/setup-compiler-macros': true,
     },
