086da036836d73875630b0376ecd391f6d491e62
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-21 11:59:55 +0100

    Prepare vueI18n vite plugin

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ssr from 'vite-plugin-ssr/plugin';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), ssr()],
+    plugins: [
+        vue(),
+        ssr(),
+        // vueI18n({
+        //     include: fileURLToPath(new URL('./src/locales/**', import.meta.url)),
+        // }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
