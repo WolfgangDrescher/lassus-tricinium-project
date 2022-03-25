3efbdf5227e97295ff9d437249e546200841d623
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-25 16:46:30 +0100

    Add Tabs component

---
+++ src/components/Tabs.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { processExpression } from '@vue/compiler-core';
+import { computed, ref } from 'vue';
+import TabPill from './TabPill.vue';
+const props = defineProps({
+    items: Object,
+});
+
+const selectedTab = ref(props.items[0].value);
+
+const slideDirection = computed(() => {
+    return (item) => {
+        return props.items.findIndex(i => selectedTab.value === i.value) < props.items.findIndex(i => item === i.value) ? 'prev' : 'next';
+    };
+});
+</script>
+
+<template>
+    <div>
+        <div class="border-b border-gray-200">
+            <div class="flex flex-wrap gap-4">
+                <TabPill v-for="item in items" :key="item.value" @click="selectedTab = item.value" :active="selectedTab === item.value">{{ item.text }}</TabPill>
+            </div>
+        </div>
+        <div class="py-4 overflow-x-hidden">
+            <template v-for="item in items" :key="item.value">
+                <div v-if="selectedTab === item.value">
+                    <slot :name="`tabItem.${item.value}`"></slot>
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
