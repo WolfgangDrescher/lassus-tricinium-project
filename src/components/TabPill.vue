0a836c046e37208d7993fb5e268271aeb2152842
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-25 16:46:08 +0100

    Add tab pill component

---
+++ src/components/TabPill.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    active: Boolean,
+});
+</script>
+
+<template>
+    <button href="#" class="py-2 px-4 border-b-2" :class="active ? 'border-primary-600 text-primary-600' : 'border-transparent hover:border-gray-300 hover:text-gray-600'">
+        <slot></slot>
+    </button>
+</template>
