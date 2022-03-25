f3211dc0bdfe88ca00b8e939bdd1570010b35902
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-03-25 16:46:38 +0100

    Use tabs

--- src/pages/tricinium/tricinium.page.vue
+++ src/pages/tricinium/tricinium.page.vue
@@ -10,6 +10,7 @@ import { useTricinium } from '../../composables/useTricinium';
 import { MeasureFilter } from '../../classes/HumdrumFilters';
 import HyperLink from '../../components/HyperLink.vue';
 import Container from '../../components/Container.vue';
+import Tabs from '../../components/Tabs.vue';
 
 const props = defineProps({
     tricinium: {
@@ -34,25 +35,64 @@ async function interactiveHumdrumScoreMounted({ callVerovioMethod }) {
 function addMeasureFilter(value) {
     interactiveHumdrumScore.value.addFilter(new MeasureFilter(value));
 }
+
+const tabItems = [
+    {
+        value: 'info',
+        text: 'Info',
+    },
+    {
+        value: 'text',
+        text: 'Text',
+    },
+    {
+        value: 'ambitus',
+        text: 'Ambitus',
+    },
+    {
+        value: 'test',
+        text: 'Test',
+    },
+];
 </script>
 
 <template>
     <Container :fluid="true">
-        <Heading>Tricinium</Heading>
-        <DataTable :items="tricinium.lyrics?.map((l, i) => ({ '#': i + 1, ...l })) || []">
-            <template #[`item.measures`]="{ item }">
-                <HyperLink href="#0" @click="addMeasureFilter(item.measures)">
-                    {{ item.measures }}
-                </HyperLink>
-            </template>
-        </DataTable>
-        <TriciniumTextDiff :tricinium="tricinium" />
-        <ClientOnly>
-            <MidiPlayer :url="audioDataUrl" />
-            <Suspense>
-                <InteractiveHumdrumScore ref="interactiveHumdrumScore" :url="tricinium.rawFile" @mounted="interactiveHumdrumScoreMounted" />
-            </Suspense>
-        </ClientOnly>
-        <!-- <pre v-text="tricinium" class="w-full overflow-y-auto"></pre> -->
+        <Heading>{{ `${tricinium.nr}. ${tricinium.title}` }}</Heading>
+        <div class="grid grid-cols-2 gap-4">
+            <div>
+                <ClientOnly>
+                    <MidiPlayer :url="audioDataUrl" />
+                    <Suspense>
+                        <InteractiveHumdrumScore ref="interactiveHumdrumScore" :url="tricinium.rawFile" @mounted="interactiveHumdrumScoreMounted" />
+                    </Suspense>
+                </ClientOnly>
+            </div>
+            <div>
+                <Tabs :items="tabItems">
+                    <template #[`tabItem.info`]>
+                        <pre v-text="tricinium" class="w-full overflow-y-auto"></pre>
+                    </template>
+                    <template #[`tabItem.text`]>
+                        <DataTable :items="tricinium.lyrics?.map((l, i) => ({ '#': i + 1, ...l })) || []">
+                            <template #[`item.measures`]="{ item }">
+                                <HyperLink href="#0" @click="addMeasureFilter(item.measures)">
+                                    {{ item.measures }}
+                                </HyperLink>
+                            </template>
+                        </DataTable>
+                        <TriciniumTextDiff :tricinium="tricinium" />
+                    </template>
+                    <template #[`tabItem.ambitus`]>
+                        <h1>Ambitus</h1>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </template>
+                    <template #[`tabItem.test`]>
+                        <h1>Test</h1>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </template>
+                </Tabs>
+            </div>
+        </div>
     </Container>
 </template>
