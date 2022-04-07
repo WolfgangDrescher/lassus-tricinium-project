a4e6c565d3e8b98814747407cbbf9051c9433eab
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-04-07 11:52:26 +0200

    Add notes tab

--- src/pages/tricinium/tricinium.page.vue
+++ src/pages/tricinium/tricinium.page.vue
@@ -95,6 +95,10 @@ const tabItems = [
                             </Suspense>
                         </ClientOnly>
                     </template>
+                    <template #[`tabItem.notes`]>
+                        <h1>Notes</h1>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </template>
                 </Tabs>
             </div>
         </div>
