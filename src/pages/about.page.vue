ece1129fdf5a9e2904c06709e6ca33aabcd92e51
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-04-07 11:50:59 +0200

    Add links to repositories

--- src/pages/about.page.vue
+++ src/pages/about.page.vue
@@ -1,10 +1,25 @@
 <script setup>
 import Heading from '../components/Heading.vue';
 import Container from '../components/Container.vue';
+import HyperLink from '../components/HyperLink.vue';
 </script>
 
 <template>
     <Container>
         <Heading>About</Heading>
+        <ul>
+            <li>
+                <HyperLink href="https://github.com/WolfgangDrescher/lassus-geistliche-psalmen">lassus-geistliche-psalmen</HyperLink>
+            </li>
+            <li>
+                <HyperLink href="https://github.com/WolfgangDrescher/ulenberg-psalmen-davids">ulenberg-psalmen-davids</HyperLink>
+            </li>
+            <li>
+                <HyperLink href="https://github.com/WolfgangDrescher/lassus-tricinium-project">lassus-tricinium-project</HyperLink>
+            </li>
+            <li>
+                <HyperLink href="https://github.com/WolfgangDrescher/vue-verovio-canvas">vue-verovio-canvas</HyperLink>
+            </li>
+        </ul>
     </Container>
 </template>
