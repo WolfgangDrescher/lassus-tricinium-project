98658e51c2f1a9b5813a785786e0786540a055ce
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-04-26 16:12:04 +0200

    Fix route path check

--- src/components/StatisticsSidebarItem.vue
+++ src/components/StatisticsSidebarItem.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { usePageContext } from '../composables/usePageContext';
 import Link from './Link.vue';
-const pageContext = usePageContext();
 </script>
 
 <template>
@@ -9,7 +7,7 @@ const pageContext = usePageContext();
         <Link
             v-bind="{ ...$attrs }"
             class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100"
-            :class="{ active: pageContext.urlPathname === $attrs.href }"
+            :class="{ active: $route.fullPath === $attrs.href }"
         >
             <span class="flex-1 ml-3 whitespace-nowrap">
                 <slot></slot>
