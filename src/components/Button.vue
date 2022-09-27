998f8367ba46a8994a9048fa3ac84daeaf839ab3
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-09-27 19:27:22 +0200

    Add full width buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,14 @@
 <template>
-    <button class="bg-primary-700 transition duration-150 ease-in-out hover:bg-primary-400 rounded text-white hover:text-black px-6 py-2 text-xs">
+    <button :class="block ? 'w-full': ''" class="bg-primary-700 transition duration-150 ease-in-out hover:bg-primary-400 rounded text-white hover:text-black px-6 py-2 text-xs">
         <slot></slot>
     </button>
 </template>
+
+<script setup>
+defineProps({
+    block: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
