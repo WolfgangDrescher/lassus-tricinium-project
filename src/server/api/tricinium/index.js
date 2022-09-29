f512ebc4e537658941b1e70079f1a301be6a810c
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-09-29 11:35:37 +0200

    Remove external fetch

--- src/server/api/tricinium/index.js
+++ src/server/api/tricinium/index.js
@@ -1,4 +1,5 @@
+import tricinia from 'lassus-geistliche-psalmen/meta.json';
+
 export default defineEventHandler(async (event) => {
-    const url = 'https://raw.githubusercontent.com/WolfgangDrescher/lassus-geistliche-psalmen/master/meta.json';
-    return await $fetch(url, { parseResponse: JSON.parse });
-})
+    return tricinia;
+});
