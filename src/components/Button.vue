b15ddf452bc7d66788f158f3468fc21edd1d2283
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-08 16:50:56 +0100

    Improve button style

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :class="block ? 'w-full': ''" class="bg-primary-700 transition duration-150 ease-in-out hover:bg-primary-400 rounded text-white hover:text-black px-6 py-2 text-xs">
+    <button :class="block ? 'w-full': ''" class="text-white bg-primary-500 text-center focus:outline-none focus:border focus:border-primary-300 focus:ring focus:ring-primary-200 px-3 py-2 text-sm border-gray-300 rounded border border-transparent shadow-sm">
         <slot></slot>
     </button>
 </template>
