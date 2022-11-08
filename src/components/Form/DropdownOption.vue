361fc2f50bfb33cd4f8bd336a3ad3f3c163db39f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-08 16:49:51 +0100

    Improve styling

--- src/components/Form/DropdownOption.vue
+++ src/components/Form/DropdownOption.vue
@@ -23,9 +23,9 @@ const toggleOption = () => {
 
 <template>
     <div @click="toggleOption" class="cursor-pointer w-full border-gray-100 rounded-t border-b hover:bg-primary-100">
-        <div class="flex w-full items-center p-2 pl-2 border-transparent border-l-2 relative hover:border-primary-100" :class="{'border-primary-600 hover:border-l-primary-600': props.selected}">
+        <div class="flex w-full items-center p-1 border-transparent border-l-2 relative hover:border-primary-100" :class="{'border-primary-600 hover:border-l-primary-600 border-l-primary-600': props.selected}">
             <div class="w-full items-center flex">
-                <div class="mx-2 leading-6">{{ props.text || props.value }}</div>
+                <div class="mx-2">{{ props.text || props.value }}</div>
             </div>
         </div>
     </div>
