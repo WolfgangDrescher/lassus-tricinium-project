a3835992b3901b1db20f66657b283d0b92693a5e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-08 16:53:25 +0100

    Refactor dropdown component

--- src/components/Form/Dropdown.vue
+++ src/components/Form/Dropdown.vue
@@ -31,7 +31,17 @@ const props = defineProps({
 
 const dropdownOptions = ref();
 
-onClickOutside(dropdownOptions, closeDropdown);
+function hasParentWithMatchingSelector(target, selector) {
+    const myArray = Array.from(document.querySelectorAll(selector));
+    return [...myArray].some(el => el !== target && el.contains(target));
+}
+
+onClickOutside(dropdownOptions, (event) => {
+    if (hasParentWithMatchingSelector(event.target, '[data-dropdown-button]')) {
+        event.stopPropagation();
+    }
+    closeDropdown();
+});
 
 const isOpen = ref(false);
 const searchString = ref('');
@@ -44,15 +54,19 @@ function toggleDropdown() {
     isOpen.value = !isOpen.value;
 }
 
-function closeDropdown(e) {
-    e.stopPropagation();
+function closeDropdown() {
     if (!isOpen.value) return;
     isOpen.value = false;
 }
 
+function openDropdown() {
+    if (isOpen.value) return;
+    isOpen.value = true;
+}
+
 function toggleOption(value) {
-    if(props.multiple) {
-        if(props.modelValue.includes(value)) {
+    if (props.multiple) {
+        if (props.modelValue.includes(value)) {
             emit('update:modelValue', props.modelValue.filter(v => v !== value));
         } else {
             emit('update:modelValue', [...props.modelValue, value]);
@@ -63,7 +77,7 @@ function toggleOption(value) {
 }
 
 function removeOption(value) {
-    if(props.multiple) {
+    if (props.multiple) {
         emit('update:modelValue', props.modelValue.filter(v => v !== value));
     } else {
         emit('update:modelValue', null);
@@ -93,37 +107,40 @@ const filteredOptions = computed(() => {
 </script>
 
 <template>
-        <div class="flex flex-col items-center">
-            <div class="w-full">
-                <div class="flex flex-col items-center relative">
-                    <div class="w-full">
-                        <div class="p-1 flex border border-gray-200 bg-white rounded">
-                            <div class="flex flex-auto flex-wrap">
-                                <div v-if="!props.modelValue?.length && props.emptyValuePlaceholder" class="p-1 px-2 text-gray-500 whitespace-nowrap" v-text="props.emptyValuePlaceholder"></div>
-                                <template v-if="props.multiple">
-                                    <FormDropdownBadge v-for="(value, index) in props.modelValue" :key="index" :text="getOptionTextFromValue(value)" :value="value" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
-                                </template>
-                                <FormDropdownBadge v-else-if="props.modelValue" :text="getOptionTextFromValue(props.modelValue)" :value="props.modelValue" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
-                                <div v-if="props.searchEnabled" class="flex-1">
-                                    <input v-model="searchString" placeholder="" class="bg-transparent p-1 px-2 appearance-none outline-none h-full w-full text-gray-800">
-                                </div>
-                            </div>
-                            <div class="text-gray-300 w-8 border-l flex items-center border-gray-200">
-                                <button @click="toggleDropdown" class="cursor-pointer w-6 h-6 text-gray-600 outline-none focus:outline-none">
-                                    <Icon v-if="isOpen" name="heroicons-solid:chevron-up"/>
-                                    <Icon v-else name="heroicons-solid:chevron-down" />
-                                </button>
-                            </div>
-                        </div>
     <FormGroup>
+        <div @click="openDropdown" class="relative">
+            <div :class="isOpen ? 'focus' : ''" class="dropdown text-gray-600 bg-white font-normal w-full flex items-center pl-3 pr-0 py-2 text-sm border-gray-300 rounded border shadow-sm">
+                <div class="w-full">
+                    <div v-if="!props.modelValue?.length && props.emptyValuePlaceholder" class="text-gray-500 whitespace-nowrap" v-text="props.emptyValuePlaceholder"></div>
+                    <div v-if="(!props.multiple && props.modelValue) || (props.multiple && props.modelValue.length)" class="pl-3 pr-0 py-2 absolute top-1/2 -translate-y-1/2 left-0 flex flex-auto flex-wrap">
+                        <template v-if="props.multiple">
+                            <FormDropdownBadge v-for="(value, index) in props.modelValue" :key="index" :text="getOptionTextFromValue(value)" :value="value" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
+                        </template>
+                        <FormDropdownBadge v-else-if="props.modelValue" :text="getOptionTextFromValue(props.modelValue)" :value="props.modelValue" @removeOption="removeOption" :show-remove-button="props.badgeShowRemoveButton" />
                     </div>
-                    <div v-if="isOpen" ref="dropdownOptions" class="absolute shadow border border-gray-200 mt-2 top-full bg-white z-40 w-full lef-0 rounded max-h-80 overflow-y-auto">
-                        <div class="flex flex-col w-full">
-                            <FormDropdownOption v-for="(option, index) in filteredOptions" :key="index" :value="option.value" :text="option.text" :selected="optionIsSelected(option.value)" @toggleOption="toggleOption" />
-                        </div>
+                </div>
+                <div class="text-gray-300 border-l flex items-center border-gray-200">
+                    <button @click="toggleDropdown" data-dropdown-button class="cursor-pointer text-gray-600 outline-none focus:outline-none px-1">
+                        <Icon v-if="isOpen" name="heroicons-solid:chevron-up" />
+                        <Icon v-else name="heroicons-solid:chevron-down" />
+                    </button>
+                </div>
+            </div>
+            <div v-if="isOpen" ref="dropdownOptions" class="absolute shadow-sm border border-gray-200 mt-2 top-full bg-white z-40 w-full left-0 rounded max-h-80 overflow-y-auto">
+                <div class="flex flex-col w-full">
+                    <div v-if="props.searchEnabled" class="p-2">
+                        <FormInputField v-model="searchString" placeholder="Search…"></FormInputField>
                     </div>
+                    <FormDropdownOption v-for="(option, index) in filteredOptions" :key="index" :value="option.value" :text="option.text" :selected="optionIsSelected(option.value)" @toggleOption="toggleOption" />
                 </div>
             </div>
         </div>
     </FormGroup>
 </template>
+
+<style scoped>
+.dropdown.focus,
+.dropdown:focus {
+    @apply outline-none border border-primary-300 ring ring-primary-200;
+}
+</style>
