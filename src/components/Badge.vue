da59b3f225d1ee288c8593c8f92b25faa6268495
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-08 17:27:00 +0100

    Smaller badges

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -19,7 +19,7 @@ function remove() {
 </script>
 
 <template>
-    <div class="relative py-2 px-4 leading-3 text-primary-700 rounded-full bg-primary-100 border border-primary-200" :class="{[`text-${textSize}`]: true, 'pr-8': removable}">
+    <div class="relative py-1 px-3 leading-3 text-primary-700 rounded-full bg-primary-100 border border-primary-200" :class="{[`text-${textSize}`]: true, 'pr-8': removable}">
         <slot></slot>
         <button v-if="removable" @click="remove" class="absolute h-full aspect-square top-0 right-0 bg-primary-100 border-l border-primary-200 hover:bg-red-300 hover:text-red-600 rounded-r-full flex justify-center items-center">
             <Icon name="heroicons-solid:x-mark" />
