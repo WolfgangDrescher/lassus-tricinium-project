abac1ea0330733e6cb5cadef921fb6b6c0a84f29
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-21 23:38:11 +0100

    Add comments to tricinium tab

--- pages/tricinium/[id].vue
+++ pages/tricinium/[id].vue
@@ -48,8 +48,8 @@ const tabItems = [
         text: t('ulenberg'),
     },
     {
-        value: 'notes',
-        text: t('notes'),
+        value: 'comments',
+        text: t('comments'),
     },
 ];
 
@@ -156,9 +156,10 @@ const useMordernClefs = ref(false);
                         </ClientOnly>
                     </template>
 
-                    <template #[`tabItem.notes`]>
-                        <h1>Notes</h1>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <template #[`tabItem.comments`]>
+                        <ul class="grid gap-2" v-if="tricinium.comments.length">
+                            <li v-for="(comment, index) in tricinium.comments" :key="index">{{ comment }}</li>
+                        </ul>
                     </template>
 
                 </Tabs>
