237f0af8205bb20968f3b03ff6842c47df09a390
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-21 13:37:55 +0100

    Add section with literature

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,31 @@
 <template>
     <Container>
-        <Heading>About</Heading>
+        <SectionContainer>
+            <section>
+                <Heading>About</Heading>
+            </section>
+            <section>
+                <h2>Literatur</h2>
+                <ul class="grid gap-4">
+                    <li>
+                        Lipphardt, Walther: «Vorwort», in: <i>Geistliche Psalmen mit dreyen Stimmen</i>, hrsg. von Walther Lipphardt, Kassel und Basel: Bärenreiter-Verlag, 1942, S. 2.
+                    </li>
+                    <li>
+                        Menke, Johannes: «Kontrapunkt I: Die Musik der Renaissance», in: <i>Grundlagen der Musik</i>, hrsg. von Felix Diergarten und Manuel Gervink, Bd. 2, Laaber: Laaber-Verlag, 2015.
+                    </li>
+                    <li>
+                        Orlandus und Rudolphus Lassus: <i>Geistliche Psalmen mit dreyen Stimmen. welche nit allain lieblich zu singen sondern auch auff aller hand art Instrumenten zu gebrauchen</i>, hrsg. von Walther Lipphardt, Kassel und Basel: Bärenreiter-Verlag, 1928, url: <HyperLink href="https://mdz-nbn-resolving.de/details:bsb00075346">https://mdz-nbn-resolving.de/details:bsb00075346</HyperLink> (besucht am 21.11.2022).
+                    </li>
+                    <li>
+                        Osthoff, Helmuth: <i>Die Niederländer und das deutsche Lied (1400–1640)</i>, Tutzing: Hans Schneider Verlag, 1967.
+                    </li>
+                    <li>
+                        Ulenberg, Caspar: <i>Die Psalmen Davids. in allerlei Teutsche gesangreimen bracht</i>, Cöln: Calenius & Quentel, 1582, url: <HyperLink href="https://mdz-nbn-resolving.de/details:bsb11116252">https://mdz-nbn-resolving.de/details:bsb11116252</HyperLink> (besucht am 21.11.2022).
+                    </li>
+                </ul>
+            </section>
+        </SectionContainer>
+
         <ul>
             <li>
                 <HyperLink href="https://github.com/WolfgangDrescher/lassus-geistliche-psalmen">lassus-geistliche-psalmen</HyperLink>
