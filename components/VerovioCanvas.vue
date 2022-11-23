c5ee99d3dc861e4bedbb3595a17c9e9ff4612be0
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-23 23:59:56 +0100

    Add VerovioCanvas wrapper component

---
+++ components/VerovioCanvas.vue
@@ -0,0 +1,7 @@
+<script setup>
+import VerovioCanvasComponent from 'vue-verovio-canvas';
+</script>
+
+<template>
+    <VerovioCanvasComponent v-bind="{ ...$attrs, ...$props }"></VerovioCanvasComponent>
+</template>
