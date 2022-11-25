6c7fd2207f247edcd4ae998f4b2e30bdf2a9877b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-25 11:17:09 +0100

    Add Subheading component

---
+++ components/Subheading.vue
@@ -0,0 +1,17 @@
+<template>
+    <component :is="level ? `h${level}` : 'div'" class="text-xl font-sans font-extrabold font-w mb-2">
+        <slot />
+    </component>
+</template>
+
+<script setup>
+defineProps({
+    level: {
+        type: Number,
+        default: null,
+        validator (value) {
+            return value >= 1 && value <= 6;
+        },
+    },
+});
+</script>
