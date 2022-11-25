771d3c96cdad6e5a37d5e2a30e9a36cb8a51df50
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-11-25 11:17:17 +0100

    Add TwoCols component

---
+++ components/TwoCols.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="grid grid-cols-2 gap-4">
+        <div>
+            <slot name="first"></slot>
+        </div>
+        <div>
+            <slot name="second"></slot>
+        </div>
+    </div>
+</template>
