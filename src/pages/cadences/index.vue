7fec8c91f0be1498eec5e76ac4e088837534f7e3
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-06 12:39:18 +0100

    Add cadences overview page

---
+++ src/pages/cadences/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineI18nRoute({
+    paths: {
+        de: '/kadenzen',
+    },
+});
+
+const { data: cadenceData } = await useAsyncData('cadences', () => queryContent('/cadences').find())
+const { data: triciniumData } = await useFetch('/api/tricinium');
+const tricinia = useTricinium(triciniumData.value);
+
+function findTricinium(id) {
+    return tricinia.find(t => t.id === id);
+}
+</script>
+
+<template>
+    <Container>
+        <Heading>{{ $t('cadences') }}</Heading>
+        <div class="grid grid-cols-2 gap-4">
+            <div v-for="cadence in cadenceData" :key="cadence._path">
+                <CadenceListItem :cadence="cadence" :tricinium="findTricinium(cadence.triciniumId)" />
+            </div>
+        </div>
+    </Container>
+</template>
