e85a9a0d44097cc83b37c56ffde1d356f1f4512d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-08 00:27:42 +0100

    Use internal component

--- src/components/InteractiveHumdrumScore.vue
+++ src/components/InteractiveHumdrumScore.vue
@@ -63,5 +63,5 @@ defineExpose({
         v-model.lazy="filtersAsString"
         class="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
     ></textarea> -->
-    <VerovioCanvasComponent ref="verovioCanvas" v-bind="verovioCanvasOptions" />
+    <VerovioCanvas ref="verovioCanvas" v-bind="verovioCanvasOptions" />
 </template>
