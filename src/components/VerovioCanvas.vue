aace6642c6e07a9ab5f24ad684c18135335c3e02
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-08 00:27:24 +0100

    Expose verovioCanvas ref

--- src/components/VerovioCanvas.vue
+++ src/components/VerovioCanvas.vue
@@ -2,7 +2,13 @@
 import 'vue-verovio-canvas/style.css'
 import { VerovioCanvas, createWorkerVerovioToolkit } from 'vue-verovio-canvas';
 import createVerovioWorker from '../workers/verovio.js?worker';
+
 const toolkit = createWorkerVerovioToolkit(createVerovioWorker())
+const verovioCanvas = ref();
+
+defineExpose({
+    verovioCanvas,
+});
 </script>
 
 <template>
