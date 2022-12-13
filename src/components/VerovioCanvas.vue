06ef4d9eddaa67ac79da16b38bf699301de2d0e7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-13 22:51:35 +0100

    Use worker from pinia store

--- src/components/VerovioCanvas.vue
+++ src/components/VerovioCanvas.vue
@@ -1,9 +1,9 @@
 <script setup>
-import 'vue-verovio-canvas/style.css'
+import 'vue-verovio-canvas/style.css';
 import { VerovioCanvas, createWorkerVerovioToolkit } from 'vue-verovio-canvas';
-import createVerovioWorker from '../workers/verovio.js?worker';
 
-const toolkit = createWorkerVerovioToolkit(createVerovioWorker())
+const { verovioWorker } = useVerovioWorker();
+const toolkit = createWorkerVerovioToolkit(verovioWorker);
 const verovioCanvas = ref();
 
 defineExpose({
