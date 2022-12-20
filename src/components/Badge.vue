8c0ac639c8af34955fbdc2c6e7afa17e8500ee4a
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-12-20 11:45:07 +0100

    Fix square aspect ratio

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -21,7 +21,7 @@ function remove() {
 <template>
     <div class="relative py-1 px-3 leading-3 text-primary-700 rounded-full bg-primary-100 border border-primary-200" :class="{[`text-${textSize}`]: true, 'pr-8': removable}">
         <slot></slot>
-        <button v-if="removable" @click="remove" class="absolute h-full aspect-square top-0 right-0 bg-primary-100 border-l border-primary-200 hover:bg-red-300 hover:border-l-transparent hover:ring-1 hover:ring-red-500 hover:border-red-400 hover:text-red-600 rounded-r-full flex justify-center items-center">
+        <button v-if="removable" @click="remove" class="absolute h-full aspect-[1/1] top-0 right-0 bg-primary-100 border-l border-primary-200 hover:bg-red-300 hover:border-l-transparent hover:ring-1 hover:ring-red-500 hover:border-red-400 hover:text-red-600 rounded-r-full flex justify-center items-center">
             <Icon name="heroicons-solid:x-mark" size=".75em" />
         </button>
     </div>
