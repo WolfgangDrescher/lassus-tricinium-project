f776b16cd44c71e06209340e70c21e022d54da9f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-09 21:39:27 +0100

    Text improvements

--- src/pages/critical-report.vue
+++ src/pages/critical-report.vue
@@ -13,60 +13,66 @@ defineI18nRoute({
                 <Heading>{{ $t('criticalReport') }}</Heading>
                 <ol class="flex flex-col list-none pl-0 gap-4">
                     <li>
-                        Vorlage ist das Digitalisat des Drucks von 1588 der Bayerische Staatsbibliothek: <HyperLink href="https://mdz-nbn-resolving.de/details:bsb00075346">https://mdz-nbn-resolving.de/details:bsb00075346</HyperLink>.
+                        Vorlage ist das Digitalisat des Drucks von 1588 in der
+                        Bayerischen Staatsbibliothek München: <HyperLink
+                        href="https://mdz-nbn-resolving.de/details:bsb00075346">https://mdz-nbn-resolving.de/details:bsb00075346</HyperLink>.
                     </li>
                     <li>
-                        Taktstriche wurden gemäss der Taktart hinzugefügt weil
+                        Taktstriche wurden gemäss der Taktart hinzugefügt, weil
                         Verovio ohne Taktstriche keine Zeilenumbrüche darstellen
                         kann.
                     </li>
                     <li>
-                        Der Liedtext orientiert sich so weit wie möglich am
-                        Druck von 1588.
+                        Die Orthographie der gesungenen Texte folgt so weit wie
+                        möglich dem Druck von 1588.
                     </li>
                     <li>
-                        Vorzeichen werden ausschliesslich innerhalb des
-                        Notentextes angezeigt, wenn diese auch im Druck
-                        vorhanden sind. Alle vom Herausgeber hinzugefügten Vorzeichen werden mit dem <code>i</code>
+                        Vorzeichen werden nur dann im Notentext angezeigt, wenn
+                        diese auch im Druck vorhanden sind. Alle vom Herausgeber
+                        hinzugefügten Vorzeichen werden mit dem <code>i</code>
                         Marker innerhalb der <code>**kern</code> Spines
                         gekennzeichnet (<code>!!!RDF**kern: i = editorial
-                        accidental</code>) und sind standardmässig oberhalb der
-                        Noten dargestellt.
+                        accidental</code>) und sind oberhalb der Noten
+                        positioniert.
                     </li>
                     <li>
-                        Vorzeichen die im Druck z.B. mit einem Kreuz erhöht
-                        werden aber wegen Tonartenvorzeichen eigentlich ein
-                        Auflösungszeichen sein sollten wurden entsprechend
-                        angepasst ohne <code>i</code> Marker für ein
+                        Vorzeichen – vor allem Kreuze – , die als
+                        Auflösungszeichen zu lesen sind, wurden entsprechend
+                        angepasst, ohne <code>i</code> Marker für ein
                         <i>editorial accidental</i>.
                     </li>
                     <li>
-                        Im Liedtext wurden ausschliesslich Symbole modernisiert
-                        die nicht mit der Standardschrift (Times) für Text in
-                        Verovio dargestellt werden können. So wurde z.B.
-                        <code>uͤ</code> zu <code>ü</code> angepasst.
+                        In den Liedtexten wurden ausschliesslich solche
+                        Schriftzeichen modernisiert, die nicht mit der
+                        Standardschrift für Verovio (Times) dargestellt werden
+                        können, wie z.B <code>uͤ</code> zu <code>ü</code>.
                     </li>
                     <li>
-                        Unterschiede im Liedtext der jeweiligen Stimmen wurden
-                        nicht angepasst und sind wie im Druck dargestellt. Dies
-                        gilt auch für Gross- und Kleinschreibung.
+                        Orthographische oder sprachliche Unterschiede in den
+                        gesungenen Texten der jeweiligen Stimmen wurden nicht
+                        angeglichen und sind wie im Druck dargestellt. Dies gilt
+                        auch für Gross- und Kleinschreibung.
                     </li>
                     <li>
                         TODO: Es wurden fehlende Schrägstriche bei den
-                        Zeilenenden sowie bei Textwiederholung hinzugefügt.
+                        Zeilenenden sowie bei einer Textwiederholung
+                        hinzugefügt.
                     </li>
                     <li>
-                        TODO: Der <i>cantus firmus</i> ist mit <i>c.f.</i> in der jeweiligen Stimme gekennzeichnet.
+                        TODO: Der <i>cantus firmus</i> ist mit <i>c.f.</i> in
+                        der entsprechenden Stimme gekennzeichnet.
                     </li>
                     <li>
-                        Bei <i>ij</i>-Passagen wird der Text ausgeschrieben, er ist aber in den Noten kurisv dargestellt.
+                        Bei Textwiederholungen, die im Druck mit <i>ij</i>
+                        gekennzeichnet sind, wird der Text kursiv
+                        ausgeschrieben.
                     </li>
                     <li>
                         <HyperLink :href="localePath({ name: 'tricinium-id',
                         params: { id: '01-beatus-vir' }})">1. Beatus
-                        vir</HyperLink> Takt 1-6: Die ausgefüllten Noten im Cantus in
-                        der ersten zwei Zeilen wurden angeglichen. Vergleiche
-                        mit
+                        vir</HyperLink> Takt 1-6: Die ausgefüllten Noten in den
+                        ersten zwei Zeilen des Cantus wurden angeglichen.
+                        Vergleiche mit
                         <HyperLink
                         href="https://www.digitale-sammlungen.de/de/view/bsb00072990?page=11">dieser
                         Alternative</HyperLink> (Permalink: <HyperLink href="https://mdz-nbn-resolving.de/details:bsb00072990">https://mdz-nbn-resolving.de/details:bsb00072990</HyperLink>).
