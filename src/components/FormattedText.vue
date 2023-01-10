83a90d041b41aedb23f8e9f0acacc3c31e33f6ee
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-10 13:49:15 +0100

    Add FormattedText component

---
+++ src/components/FormattedText.vue
@@ -0,0 +1,10 @@
+<template>
+    <slot></slot>
+</template>
+
+<style scoped>
+:slotted(code) {
+    @apply bg-[rgba(175,184,193,0.2)] rounded-md text-[80%] m-0 px-[.4em] py-[.2em];
+    white-space: break-spaces;
+}
+</style>
