bacfecdec089f03192efec0aa689167649f70f36
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-21 15:53:24 +0100

    Add FiguredbassFilter to filter configurator

--- src/components/Humdrum/FilterConfigurator.vue
+++ src/components/Humdrum/FilterConfigurator.vue
@@ -3,6 +3,7 @@ import InputField from '../Form/InputField.vue';
 import FormGroup from '../Form/Group.vue';
 import Button from '../Button.vue';
 import Dropdown from '../Form/Dropdown.vue';
+import Checkbox from '../Form/Checkbox.vue';
 import ColorPicker from '../Form/ColorPicker.vue';
 import { HumdrumFilter, TransposeFilter } from '../../classes/HumdrumFilters.js';
 
@@ -21,6 +22,7 @@ const props = defineProps({
 // TransposeFilter
 // SicFilter
 // CustomFilter
+// FiguredbassFilter
 
 const emit = defineEmits(['applyFilter']);
 
@@ -38,6 +40,26 @@ function convertArgsToArray() {
 
     if (props.filter === 'TransposeFilter') {
         return [args.mode, args.value];
+    }
+
+    if (props.filter === 'FiguredbassFilter') {
+        return [
+            args.compound,
+            args.accidentals,
+            args.intervallsatz,
+            args.sort,
+            args.lowest,
+            args.normalize,
+            args.abbr,
+            args.ties,
+            args.hideThree,
+            args.negative,
+            args.above,
+            args.baseTrack,
+            args.frequency,
+            args.kernTracks,
+            args.spineTracks,
+        ];
     }
 
     return Object.entries(args).map(([_, value]) => {
@@ -61,6 +83,24 @@ const DynamicHumdrumFilter = defineComponent({
                 // mode: TransposeFilter.MODE_INTERVAL,
                 mode: undefined,
                 value: undefined,
+            });
+        } else if (props.filter === 'FiguredbassFilter') {
+            Object.assign(args, {
+                compound: undefined,
+                accidentals: undefined,
+                intervallsatz: undefined,
+                sort: undefined,
+                lowest: undefined,
+                normalize: undefined,
+                abbr: undefined,
+                ties: undefined,
+                hideThree: undefined,
+                negative: undefined,
+                above: undefined,
+                baseTrack: undefined,
+                frequency: undefined,
+                kernTracks: undefined,
+                spineTracks: undefined,
             });
         } else {
             Object.assign(args, {});
@@ -175,6 +215,83 @@ const DynamicHumdrumFilter = defineComponent({
                             ],
                             multiple: false,
                             searchEnabled: false,
+                        }),
+                    ];
+                    break;
+                case 'FiguredbassFilter':
+                    elems = [
+                        h(Checkbox, {
+                            modelValue: args.compound,
+                            ['onUpdate:modelValue']: value => { args.compound = value; },
+                            label: t('fb.compound'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.accidentals,
+                            ['onUpdate:modelValue']: value => { args.accidentals = value; },
+                            label: t('fb.accidentals'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.intervallsatz,
+                            ['onUpdate:modelValue']: value => { args.intervallsatz = value; },
+                            label: t('fb.intervallsatz'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.sort,
+                            ['onUpdate:modelValue']: value => { args.sort = value; },
+                            label: t('fb.sort'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.lowest,
+                            ['onUpdate:modelValue']: value => { args.lowest = value; },
+                            label: t('fb.lowest'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.normalize,
+                            ['onUpdate:modelValue']: value => { args.normalize = value; if (value) { args.compound = true; args.sort = true; } },
+                            label: t('fb.normalize'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.abbr,
+                            ['onUpdate:modelValue']: value => { args.abbr = value; if (value) { args.normalize = true; args.compound = true; args.sort = true; } },
+                            label: t('fb.abbr'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.ties,
+                            ['onUpdate:modelValue']: value => { args.ties = value; },
+                            label: t('fb.ties'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.hideThree,
+                            ['onUpdate:modelValue']: value => { args.hideThree = value; },
+                            label: t('fb.hideThree'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.negative,
+                            ['onUpdate:modelValue']: value => { args.negative = value; },
+                            label: t('fb.negative'),
+                        }),
+                        h(Checkbox, {
+                            modelValue: args.above,
+                            ['onUpdate:modelValue']: value => { args.above = value; },
+                            label: t('fb.above'),
+                        }),
+                        h(Dropdown, {
+                            modelValue: args.baseTrack,
+                            ['onUpdate:modelValue']: value => { args.baseTrack = value; },
+                            label: t('fb.baseTrack'),
+                            options: [
+                                { value: '1', text: t('voice.bassus') },
+                                { value: '2', text: t('voice.tenor') },
+                                { value: '3', text: t('voice.cantus') },
+                            ],
+                            multiple: false,
+                            searchEnabled: false,
+                        }),
+                        h(InputField, {
+                            modelValue: args.frequency,
+                            ['onUpdate:modelValue']: value => { args.frequency = value; },
+                            label: t('fb.frequency'),
+                            placeholder: '4; 4.; 8; 2',
                         }),
                     ];
                     break;
