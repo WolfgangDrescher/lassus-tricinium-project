7585f220f7e00c6b0ba80df852674c5c87731a3f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-21 15:42:13 +0100

    Add FormCheckbox component

---
+++ src/components/Form/Checkbox.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['label', 'modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+function onChange(event) {
+    emit('update:modelValue', event.target.value);
+}
+</script>
+
+<template>
+    <FormGroup>
+        <label>
+            <input type="checkbox" @change="onChange" :value="modelValue"/>
+            {{ label }}
+        </label>
+    </FormGroup>
+</template>
