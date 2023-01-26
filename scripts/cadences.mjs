695fbcf55b047787ab1d139266170e8449dfa3df
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 12:03:30 +0100

    Use `myank` to extract cadences

--- scripts/cadences.mjs
+++ scripts/cadences.mjs
@@ -100,7 +100,7 @@ getFiles(`${__dirname}/../lassus-geistliche-psalmen/kern`).forEach(file => {
                 const degree = getCadenceDegree(ultima, getFinalisFromFile(file));
                 const endLine = kern.substring(0, endResult.index).split('\n').length;
 
-                const stdout = execSync(`cat ${file} | tie -s |  yank -c -l -r ${startLine}-${endLine}`).toString();
+                const stdout = execSync(`cat ${file} | myank -I -l ${startLine}-${endLine}`).toString();
                 const cadenceFileLines = stdout.split('\n').filter(line => {
                     return !(line.match(/^!!\s?cadence/) || line.includes('*xywh'));
                 });
