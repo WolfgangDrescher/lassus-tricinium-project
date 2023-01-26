82addd823987ac68d88ef5b80c3d16e5e13cde0d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 22:13:07 +0100

    Add mh freiburg section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,16 @@
                     / und seinem Sohn Rudolphum newlich componirt / und in Truck verfertiget.
                 </ImageCaption>
             </section>
+            <section>
+                <TwoCols>
+                    <template v-slot:first>
+                        <img src="/mhfr-logo.svg" class="max-h-[150px] mx-auto" alt="Hochschule für Musik Freiburg" />
+                    </template>
+                    <template v-slot:second>
+                        <img src="/fzm-logo.jpg" class="max-h-[150px] mx-auto" alt="FZM – Freiburger Forschungs- und Lehrzentrum Musik" />
+                    </template>
+                </TwoCols>
+            </section>
         </SectionContainer>
     </Container>
 </template>
