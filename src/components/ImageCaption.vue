ce82f808f0587ace54e233db20104929f2386354
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 21:08:35 +0100

    Add ImageCaption component

---
+++ src/components/ImageCaption.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    noMx: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <div class="my-4 italic text-sm text-center" :class="{'mx-0 sm:mx-4 md:mx-8 lg:mx-12': !noMx}">
+        <slot></slot>
+    </div>
+</template>
