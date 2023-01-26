cfd6a5312a899bcd0f1630eeff6ce46a4e98e66d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 22:01:53 +0100

    Make font configurable

--- src/components/Subheading.vue
+++ src/components/Subheading.vue
@@ -1,9 +1,3 @@
-<template>
-    <component :is="level ? `h${level}` : 'div'" class="text-xl font-sans font-extrabold font-w mb-2">
-        <slot />
-    </component>
-</template>
-
 <script setup>
 defineProps({
     level: {
@@ -13,5 +7,22 @@ defineProps({
             return value >= 1 && value <= 6;
         },
     },
+    fontWeight: {
+        type: String,
+        default: 'extrabold',
+    },
+    fontSize: {
+        type: String,
+        default: 'xl',
+    },
 });
+// font-thin font-extralight font-light font-normal font-medium font-semibold font-bold font-extrabold font-black
+// text-xs text-sm text-base text-lg text-xl text-2xl text-3xl text-4xl text-5xl text-6xl text-7xl text-8xl text-9xl
 </script>
+
+<template>
+    <component :is="level ? `h${level}` : 'div'" class="font-sans font-w mb-2" :class="`font-${fontWeight} text-${fontSize}`">
+        <slot />
+    </component>
+</template>
+
