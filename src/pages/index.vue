b2a8b2700dd0d148793b27d20045e3f7db11d04b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 22:12:26 +0100

    Add intro section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
     <Container>
-        <Heading>Home</Heading>
+        <SectionContainer>
+            <section>
+                <Heading>Home</Heading>
+                <img src="/lasso-geistliche-psalmen-cover.jpg" class="w-full shadow" alt="Geistliche Psalmen / mit dreyen stimmen / welche nit allain lieblich zu singen / sondern auch auff aller hand art Instrumenten zugebrauchen." />
+                <ImageCaption>
+                    Geistliche Psalmen / mit dreyen stimmen / welche nit allain lieblich zu singen / sondern auch auff
+                    aller hand art Instrumenten zugebrauchen. Durch Orlandum de Lasso, Fürst: Bay: Obristen Capelmeister
+                    / und seinem Sohn Rudolphum newlich componirt / und in Truck verfertiget.
+                </ImageCaption>
+            </section>
+        </SectionContainer>
     </Container>
 </template>
