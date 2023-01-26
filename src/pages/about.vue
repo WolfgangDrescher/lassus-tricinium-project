6aafc3a4a4e40f1f766602b2abc32a18039ebb16
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 21:07:03 +0100

    Remove links to github repos

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -25,20 +25,5 @@
                 </ul>
             </section>
         </SectionContainer>
-
-        <ul>
-            <li>
-                <HyperLink href="https://github.com/WolfgangDrescher/lassus-geistliche-psalmen">lassus-geistliche-psalmen</HyperLink>
-            </li>
-            <li>
-                <HyperLink href="https://github.com/WolfgangDrescher/ulenberg-psalmen-davids">ulenberg-psalmen-davids</HyperLink>
-            </li>
-            <li>
-                <HyperLink href="https://github.com/WolfgangDrescher/lassus-tricinium-project">lassus-tricinium-project</HyperLink>
-            </li>
-            <li>
-                <HyperLink href="https://github.com/WolfgangDrescher/vue-verovio-canvas">vue-verovio-canvas</HyperLink>
-            </li>
-        </ul>
     </Container>
 </template>
