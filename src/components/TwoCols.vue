8880d37c8bf698c91a18c48bcaac85a29201a4e1
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-26 21:08:24 +0100

    Improve TwoCols component

--- src/components/TwoCols.vue
+++ src/components/TwoCols.vue
@@ -1,10 +1,27 @@
+<script setup>
+defineProps({
+    reverse: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
 <template>
     <div class="grid grid-cols-2 gap-4">
-        <div>
-            <slot name="first"></slot>
+        <div :class="reverse ? 'md:order-2' : ''">
+            <div class="h-full flex items-center">
+                <div class="w-full">
+                    <slot name="first"></slot>
+                </div>
+            </div>
         </div>
-        <div>
-            <slot name="second"></slot>
+        <div :class="reverse ? 'md:order-1' : ''">
+            <div class="h-full flex items-center">
+                <div class="w-full">
+                    <slot name="second"></slot>
+                </div>
+            </div>
         </div>
     </div>
 </template>
