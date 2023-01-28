aa9566a70e7583c1df7157db5acbc31aa20725db
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-28 20:41:21 +0100

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div class="p-6 shadow border border-gray-100 rounded">
+        <div class="border-b border-gray-200 pb-3 mb-3">
+            <slot name="title">
+                <div class="text-xl font-medium leading-5 text-gray-800">
+                    {{ title }}
+                </div>
+            </slot>
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
