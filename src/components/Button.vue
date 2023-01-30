1a29af5dac068a44222dacf02a1eb37f68aae4db
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-30 14:44:06 +0100

    Make hover and focus styles configurable

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,3 @@
-<template>
-    <button :class="dynamicClasses" class=" text-center focus:outline-none focus:border focus:border-primary-300 focus:ring focus:ring-primary-200 px-3 py-2 text-sm border-gray-300 rounded border border-transparent shadow-sm">
-        <slot></slot>
-    </button>
-</template>
-
 <script setup>
 const props = defineProps({
     block: {
@@ -18,13 +12,25 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    hover: {
+        type: Boolean,
+        default: true,
+    },
 });
 
 const dynamicClasses = computed(() => {
     return {
-        'w-full': props.block, 
+        'w-full': props.block,
         'text-white bg-primary-500': (!props.outline && !props.selected) || props.selected,
-        'bg-gray-100 border-1 border-gray-200 hover:text-white hover:bg-primary-500': props.outline && ! props.selected, 
+        'bg-gray-100 border-1 border-gray-200': props.outline && !props.selected,
+        'hover:text-white hover:bg-primary-500': props.outline && !props.selected && props.hover,
+        'focus:outline-none focus:border focus:border-primary-300 focus:ring focus:ring-primary-200': props.hover,
     };
 });
 </script>
+
+<template>
+    <button @click="onClick" :class="dynamicClasses" class="text-center px-3 py-2 text-sm border-gray-300 rounded border border-transparent shadow-sm">
+        <slot></slot>
+    </button>
+</template>
