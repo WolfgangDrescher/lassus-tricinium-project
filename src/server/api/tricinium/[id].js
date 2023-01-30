172dd00fb7eea1aaa096aeb3754ac6bb6a2fd49d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-30 10:41:53 +0100

    Return prev and next item in tricinium api

--- src/server/api/tricinium/[id].js
+++ src/server/api/tricinium/[id].js
@@ -1,11 +1,23 @@
 import { serverQueryContent } from '#content/server';
 
+function getIdFromPath(path) {
+    return path.replace(/^.*[\\\/]/, '');
+}
+
 export default defineEventHandler(async (event) => {
     const { id } = event.context.params;
-    return await serverQueryContent(event).where({
+    const tricinium = await serverQueryContent(event).where({
         _source: 'lgp',
         _path: {
             $contains: `/lgp/${id}`,
         },
     }).findOne();
+    if (tricinium) {
+        const [prev, next] = await serverQueryContent(event).where({
+            _source: 'lgp',
+        }).findSurround(tricinium._path);
+        tricinium['@prev'] = prev ? getIdFromPath(prev._path) : null;
+        tricinium['@next'] = next ? getIdFromPath(next._path) : null;
+    }
+    return tricinium;
 });
