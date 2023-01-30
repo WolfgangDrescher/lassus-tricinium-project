103b37fb91ca3998f83b7de224687672bc516b3f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-30 12:39:12 +0100

    Add showSidebar and showModernClefs to tricinium view options

--- src/composables/tricinium-view-options.js
+++ src/composables/tricinium-view-options.js
@@ -4,6 +4,8 @@ const defaultFilter = {
     viewMode: 'score',
     scoreDisplay: 'lassus',
     showLyrics: true,
+    showSidebar: true,
+    showModernClefs: true,
 };
 
 export const useTriciniumViewOptionsStore = defineStore('tricinium_view_options', {
