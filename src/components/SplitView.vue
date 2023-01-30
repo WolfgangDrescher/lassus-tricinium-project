4c826e95860b57985de90ffc3e66a314e56aec4d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-30 12:40:10 +0100

    Add support to hide a side of the split view

--- src/components/SplitView.vue
+++ src/components/SplitView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { useDrag } from '@vueuse/gesture';
 
+const props = defineProps({
+    hide: String,
+});
+
 const dragHandle = ref();
 const wrapper = ref();
-
 const leftWidth = ref(50);
 
 function calcWidth(x) {
@@ -16,24 +19,26 @@ function dragHandler(dragState) {
     leftWidth.value = calcWidth(x);
 }
 
-useDrag(dragHandler, {
-    domTarget: dragHandle,
-});
+if (!props.hide) {
+    useDrag(dragHandler, {
+        domTarget: dragHandle,
+    });
+}
 </script>
 
 <template>
     <div class="flex" ref="wrapper">
-        <div class="shrink-0 grow-0" :style="{width: `${leftWidth}%`}">
+        <div v-if="hide !== 'left'" class="shrink-0 min-w-0" :class="[!hide && 'grow-0']" :style="{ width: hide ? '100%' : `${leftWidth}%`}">
             <slot name="left"></slot>
         </div>
-        <div class="shrink-0 grow-0">
+        <div v-if="!hide" class="shrink-0 grow-0">
             <div class="px-2 h-full group/separator">
                 <div class="cursor-ew-resize px-1 h-full hover:bg-red-50" ref="dragHandle">
                     <div class="h-full w-[2px] group-hover/separator:bg-gray-300"></div>
                 </div>
             </div>
         </div>
-        <div class="shrink grow min-w-0">
+        <div v-if="hide !== 'right'" class="shrink grow min-w-0">
             <slot name="right"></slot>
         </div>
     </div>
