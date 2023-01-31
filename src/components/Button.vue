c7d371763936e071abdbf42b52c8e9a6b34bccb2
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-31 20:31:48 +0100

    Add button sizes

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -16,6 +16,22 @@ const props = defineProps({
         type: Boolean,
         default: true,
     },
+    size: {
+        type: String,
+        default: 'md',
+    },
+});
+
+const sizeClasses = computed(() => {
+    switch(props.size) {
+        case 'lg':
+            return 'px-4 py-2 text-lg';
+        case 'sm':
+            return 'px-2 py-1 text-xs';
+        case 'md':
+        default:
+            return 'px-3 py-2 text-sm';
+    }
 });
 
 const dynamicClasses = computed(() => {
@@ -25,12 +41,13 @@ const dynamicClasses = computed(() => {
         'bg-gray-100 border-1 border-gray-200': props.outline && !props.selected,
         'hover:text-white hover:bg-primary-500': props.outline && !props.selected && props.hover,
         'focus:outline-none focus:border focus:border-primary-300 focus:ring focus:ring-primary-200': props.hover,
+        [sizeClasses.value]: true,
     };
 });
 </script>
 
 <template>
-    <button @click="onClick" :class="dynamicClasses" class="text-center px-3 py-2 text-sm border-gray-300 rounded border border-transparent shadow-sm">
+    <button @click="onClick" :class="dynamicClasses" class="text-center border-gray-300 rounded border border-transparent shadow-sm">
         <slot></slot>
     </button>
 </template>
