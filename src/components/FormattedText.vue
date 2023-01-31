8f1fe1e55f4b8bb69226c9e73ceff991af21a8a4
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-31 14:07:56 +0100

    Fix typo

--- src/components/FormattedText.vue
+++ src/components/FormattedText.vue
@@ -3,7 +3,7 @@
 </template>
 
 <style scoped>
-:slotted(:deeo(code)) {
+:slotted(:deep(code)) {
     @apply bg-[rgba(175,184,193,0.2)] rounded-md text-[80%] m-0 px-[.4em] py-[.2em];
     white-space: break-spaces;
 }
