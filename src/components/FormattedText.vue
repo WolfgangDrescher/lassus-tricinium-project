44afff71c5a6936c0714db2937ef1e902b22dc40
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-31 12:29:06 +0100

    Apply styles on deep slot components

--- src/components/FormattedText.vue
+++ src/components/FormattedText.vue
@@ -3,24 +3,24 @@
 </template>
 
 <style scoped>
-:slotted(code) {
+:slotted(:deeo(code)) {
     @apply bg-[rgba(175,184,193,0.2)] rounded-md text-[80%] m-0 px-[.4em] py-[.2em];
     white-space: break-spaces;
 }
 
-:slotted(p + p) {
+:slotted(:deep(p + p)) {
     @apply mt-4;
 }
 
-:slotted(h3) {
+:slotted(:deep(h3)) {
     @apply font-sans mt-6 mb-1 font-bold text-base;
 }
 
-:slotted(ul) {
+:slotted(:deep(ul)) {
     @apply list-disc ml-8 my-3;
 }
 
-:slotted(ul) li {
+:slotted(:deep(ul)) li {
     @apply my-1;
 }
 </style>
