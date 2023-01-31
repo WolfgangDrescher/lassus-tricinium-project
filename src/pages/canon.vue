0b630d9e102684f63f395f1326a2fae87e2472ea
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-01-31 11:40:33 +0100

    Add canon page

---
+++ src/pages/canon.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { t } = useI18n();
+useHead({
+    title: t('canon'),
+});
+
+defineI18nRoute({
+    paths: {
+        de: '/canon',
+    },
+});
+
+const { data } = await useAsyncData('canon', () => queryContent('/canon').findOne())
+</script>
+
+<template>
+    <Container>
+        <FormattedText>
+            <ContentRenderer :value="data" />
+        </FormattedText>
+    </Container>
+</template>
