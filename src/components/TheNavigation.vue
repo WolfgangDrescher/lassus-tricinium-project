060692b3654c3505b3825fa45274c3a2592899ea
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-02-12 23:46:42 +0100

    Improve for usage with header component

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,46 +1,43 @@
 <script setup>
+defineProps({
+    open: Boolean,
+});
+
+const emit = defineEmits(['itemSelected'])
+
 const localePath = useLocalePath();
-const isOpen = ref(false);
-function toggle() {
-    isOpen.value = !isOpen.value;
-};
-function close() {
-    isOpen.value = false;
+
+function onClick() {
+    emit('itemSelected');
 }
 </script>
 
 <template>
-    <nav class="bg-gray-100 py-5" :class="{'is-open': isOpen}">
+    <nav class="bg-gray-100 py-3 md:py-5 hidden shadow md:shadow-none" :class="{'is-open': open}" id="header-navigation">
         <Container>
-            <div class="flex flex-wrap justify-between items-center mx-auto">
-                <a href="/" class="flex">
-                    <span class="self-center text-2xl font-bold whitespace-nowrap">Lassus Tricinium Project</span>
-                </a>
-                <button
-                    type="button"
-                    @click="toggle"
-                    class="inline-flex items-center p-2 ml-3 text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                >
-                    <Icon name="heroicons-solid:bars-3" />
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-navigation">
-                    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0">
-                        <NavigationItem :href="localePath('index')" @click="close">{{ $t('home') }}</NavigationItem>
-                        <NavigationItem :href="localePath('about')" @click="close">{{ $t('about') }}</NavigationItem>
-                        <NavigationItem :href="localePath('tricinium')" @click="close">{{ $t('tricinia') }}</NavigationItem>
-                        <NavigationItem :href="localePath('cadences')" @click="close">{{ $t('cadences') }}</NavigationItem>
-                        <NavigationItem :href="localePath('statistics')" @click="close">{{ $t('statistics') }}</NavigationItem>
-                        <NavigationItem :href="localePath('critical-report')" @click="close">{{ $t('criticalReport') }}</NavigationItem>
-                        <NavigationItem :href="localePath('canon')" @click="close">{{ $t('canon') }}</NavigationItem>
-                    </ul>
-                </div>
+            <div class="w-full md:block md:w-auto">
+                <ul class="flex flex-col md:flex-row md:space-x-8">
+                    <NavigationItem :href="localePath('index')" @click="onClick">{{ $t('home') }}</NavigationItem>
+                    <NavigationItem :href="localePath('about')" @click="onClick">{{ $t('about') }}</NavigationItem>
+                    <NavigationItem :href="localePath('tricinium')" @click="onClick">{{ $t('tricinia') }}</NavigationItem>
+                    <NavigationItem :href="localePath('cadences')" @click="onClick">{{ $t('cadences') }}</NavigationItem>
+                    <NavigationItem :href="localePath('statistics')" @click="onClick">{{ $t('statistics') }}</NavigationItem>
+                    <NavigationItem :href="localePath('critical-report')" @click="onClick">{{ $t('criticalReport') }}</NavigationItem>
+                    <NavigationItem :href="localePath('canon')" @click="onClick">{{ $t('canon') }}</NavigationItem>
+                </ul>
             </div>
         </Container>
     </nav>
 </template>
 
 <style scoped>
-.is-open #navbar-navigation {
+#header-navigation.is-open {
     @apply block;
+}
+
+@screen md {
+    #header-navigation {
+        @apply block;
+    }
 }
 </style>
