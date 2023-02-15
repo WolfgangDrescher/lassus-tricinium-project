79e6b33ed8f3313134bf33315fb66957a59d33fd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-02-15 23:18:31 +0100

    Make badge selectable

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -7,6 +7,10 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    selected: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 const emit = defineEmits(['remove']);
@@ -19,7 +23,7 @@ function remove() {
 </script>
 
 <template>
-    <div class="relative py-1 px-3 leading-3 text-primary-700 rounded-full bg-primary-100 border border-primary-200" :class="{[`text-${textSize}`]: true, 'pr-7': removable}">
+    <div class="relative py-1 px-3 leading-3 rounded-full border cursor-pointer" :class="{ [`text-${textSize}`]: true, 'pr-7': removable, 'border-primary-700 text-white bg-primary-700': selected, 'border-primary-200 text-primary-700 bg-primary-100': !selected}">
         <slot></slot>
         <button v-if="removable" @click="remove" class="absolute h-full aspect-[1/1] top-0 right-0 bg-primary-100 border-l border-primary-200 hover:bg-red-300 hover:border-l-transparent hover:ring-1 hover:ring-red-500 hover:border-red-400 hover:text-red-600 rounded-r-full flex justify-center items-center">
             <Icon name="heroicons-solid:x-mark" size=".75em" />
