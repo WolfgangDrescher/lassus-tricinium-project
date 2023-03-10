f3f6fc3991d512457b7f1319a52d361a7e466ba2
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-10 23:38:29 +0100

    Gray timeline background color

--- src/components/CadenceTimeline.vue
+++ src/components/CadenceTimeline.vue
@@ -8,7 +8,7 @@ const highestBeat = Math.max(...(props.cadences || []).map(c => c.beat));
 
 <template>
     <div class="py-4">
-        <div class="h-2 bg-primary-500 rounded-full relative">
+        <div class="h-2 bg-gray-200 rounded-full relative">
             <div v-for="cadence in cadences" :key="cadence._id" class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-500 text-white w-8 h-8 flex justify-center items-center border-4 border-white" :style="{left: `${cadence.beat / highestBeat * 100}%`}">
                 {{ cadence.ultima }}
             </div>
