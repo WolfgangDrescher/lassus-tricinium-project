419a3d9c4f0a2ba11d6e20f470ccf93adfd6cae7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-10 23:31:29 +0100

    Add CadenceTimeline component

---
+++ src/components/CadenceTimeline.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+    cadences: Array,
+});
+
+const highestBeat = Math.max(...(props.cadences || []).map(c => c.beat));
+</script>
+
+<template>
+    <div class="py-4">
+        <div class="h-2 bg-primary-500 rounded-full relative">
+            <div v-for="cadence in cadences" :key="cadence._id" class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-500 text-white w-8 h-8 flex justify-center items-center border-4 border-white" :style="{left: `${cadence.beat / highestBeat * 100}%`}">
+                {{ cadence.ultima }}
+            </div>
+        </div>
+    </div>
+</template>
