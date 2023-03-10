715f3c58476ac33642c3d0dc4f35e9b3a0fe071d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-10 23:47:28 +0100

    Add z-index on hover

--- src/components/CadenceTimeline.vue
+++ src/components/CadenceTimeline.vue
@@ -9,7 +9,7 @@ const highestBeat = Math.max(...(props.cadences || []).map(c => c.beat));
 <template>
     <div class="py-4">
         <div class="h-2 bg-gray-200 rounded-full relative">
-            <div v-for="cadence in cadences" :key="cadence._id" class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-500 text-white w-8 h-8 flex justify-center items-center border-4 border-white" :style="{left: `${cadence.beat / highestBeat * 100}%`}">
+            <div v-for="cadence in cadences" :key="cadence._id" class="absolute hover:z-10 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-red-500 text-white w-8 h-8 flex justify-center items-center border-4 border-white" :style="{left: `${cadence.beat / highestBeat * 100}%`}">
                 {{ cadence.ultima }}
             </div>
         </div>
