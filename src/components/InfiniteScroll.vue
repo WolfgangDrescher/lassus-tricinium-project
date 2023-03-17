8b92d6f1e06ef6790b19b17a5f95668321e1646f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-17 22:13:16 +0100

    Add renderless InfiniteScroll component

---
+++ src/components/InfiniteScroll.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+    all: Boolean,
+});
+
+const emit = defineEmits(['load'])
+const el = ref(null);
+
+watch(() => props.all, (value) => {
+    if (value === true) {
+        window.removeEventListener('scroll', handleScroll);
+    }
+});
+
+function handleScroll(event) {
+    if (el.value.getBoundingClientRect().bottom < window.innerHeight) {
+        emit('load', event);
+    }
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+    <div ref="el">
+        <slot></slot>
+    </div>
+</template>
