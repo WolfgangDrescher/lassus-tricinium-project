08c94140e140273274441c23f2cd66fc60b7f5a7
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-17 11:10:20 +0100

    Add swiper for tab pills

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,4 +1,8 @@
 <script setup>
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const props = defineProps({
     items: Object,
 });
@@ -11,19 +15,40 @@ const slideDirection = computed(() => {
     };
 });
 
+const tabNav = ref();
+const activeIndexChange = ref(1);
+
 onMounted(() => {
     const hash = typeof location !== 'undefined' && location.hash ? location.hash.split('#')[1] : null;
     if(hash && props.items.map(item => item.value).includes(hash)) {
         selectedTab.value = hash;
     }
+    const swiper = new Swiper(tabNav.value, {
+        modules: [Navigation],
+        spaceBetween: 16,
+        slidesPerView: 'auto',
+        navigation: {
+            prevEl: tabNav.value.querySelector('.swiper-button-prev'),
+            nextEl: tabNav.value.querySelector('.swiper-button-next'),
+        },
+    });
+    swiper.on('slideChange', function () {
+        console.log('slide changed');
+    });
 });
 </script>
 
 <template>
     <div>
         <div class="border-b border-gray-200">
-            <div class="flex flex-wrap gap-4">
-                <TabPill v-for="item in items" :key="item.value" :hash="item.value" @click="selectedTab = item.value" :active="selectedTab === item.value">{{ item.text }}</TabPill>
+            <div class="swiper" ref="tabNav">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" v-for="item in items" :key="item.value">
+                        <TabPill :hash="item.value" @click="selectedTab = item.value" :active="selectedTab === item.value">{{ item.text }}</TabPill>
+                    </div>
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
             </div>
         </div>
         <div class="py-4">
@@ -35,3 +60,37 @@ onMounted(() => {
         </div>
     </div>
 </template>
+
+<style scoped>
+.swiper {
+    --swiper-theme-color: var(--color-primary-500);
+    --swiper-navigation-size: 1rem;
+    @apply relative;
+}
+
+.swiper-slide {
+    @apply w-auto;
+}
+
+.swiper-button-prev,
+.swiper-button-next {
+    --angle: 0deg;
+    @apply h-full top-0 mt-0 w-8;
+    background: rgb(255,255,255);
+    background: linear-gradient(var(--angle), rgba(255,255,255,0) 0%, rgba(255,255,255,1) 65%);
+}
+
+.swiper-button-prev {
+    --angle: -90deg;
+    @apply left-0 justify-start;
+}
+
+.swiper-button-next {
+    --angle: 90deg;
+    @apply right-0 justify-end;
+}
+
+.swiper-button-disabled {
+    @apply opacity-0;
+}
+</style>
