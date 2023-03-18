e3846c4095e9fec03d0e4e5c8c619eb47800011e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-03-18 22:02:14 +0100

    Parse iiifManifestUrl if not passed as argument

--- src/composables/iiif.js
+++ src/composables/iiif.js
@@ -1,5 +1,41 @@
+function parseHumdrumReferenceRecords(humdrum) {
+    let lines = humdrum.split(/\r?\n/);
+    let output = {};
+    for (let i = 0; i < lines.length; i++) {
+        const matches = lines[i].match(/^!!!\s*([^:]+)\s*:\s*(.*)\s*$/);
+        if (matches) {
+            const existingValue = output[matches[1]];
+            if (Array.isArray(existingValue)) {
+                output[matches[1]].push(matches[2]);
+            } else if (!Array.isArray(existingValue) && typeof existingValue !== 'undefined') {
+                output[matches[1]] = [existingValue, matches[2]];
+            } else {
+                output[matches[1]] = matches[2];
+            }
+        }
+    }
+    return output;
+}
+
+function getHumdrumReferenceRecod(humdrum, key) {
+    const refs = parseHumdrumReferenceRecords(humdrum);
+    if (Array.isArray(key)) {
+        for (let value of key) {
+            if (refs[value]) {
+                return refs[value];
+            }
+        }
+    }
+    return refs[key] || null;
+}
+
 export function useIiif(scoreData, iiifManifestUrl) {
     const lookupMap = [];
+
+    if (!iiifManifestUrl) {
+        iiifManifestUrl = getHumdrumReferenceRecod(scoreData, 'IIIF');
+    }
+
     const fetchManifestPromise = $fetch(iiifManifestUrl);
 
     function getPrevXywh(lineIndex, fieldIndex) {
