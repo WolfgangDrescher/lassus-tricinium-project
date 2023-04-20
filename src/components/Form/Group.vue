ad94c6487dfdcacc0cf2cf61ba3579234fccb8e8
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-04-21 00:32:22 +0200

    Improve group vertical alignment

--- src/components/Form/Group.vue
+++ src/components/Form/Group.vue
@@ -3,12 +3,14 @@ const props = defineProps(['label']);
 </script>
 
 <template>
-    <div class="flex flex-col w-full">
+    <div class="flex flex-col w-full h-full">
         <div v-if="label || label === ''">
             <label class="label-text text-gray-800 text-sm mb-2">{{ label }}</label>
         </div>
-        <div>
-            <slot></slot>
+        <div class="h-full flex items-center min-w-0">
+            <div class="w-full">
+                <slot></slot>
+            </div>
         </div>
     </div>
 </template>
