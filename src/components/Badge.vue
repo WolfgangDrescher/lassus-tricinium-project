69843e4b9427efea037b8db310ce04d9429e721e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-06-19 10:07:20 +0200

    Add clickable styles

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -11,6 +11,10 @@ const props = defineProps({
         type: Boolean,
         default: false,
     },
+    clickable: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 const emit = defineEmits(['remove']);
@@ -23,7 +27,14 @@ function remove() {
 </script>
 
 <template>
-    <div class="relative py-1 px-3 leading-3 rounded-full border" :class="{ [`text-${textSize}`]: true, 'pr-7': removable, 'border-primary-700 text-white bg-primary-700': selected, 'border-primary-200 text-primary-700 bg-primary-100': !selected}">
+    <div class="relative py-1 px-3 leading-3 rounded-full border" :class="{
+        [`text-${textSize}`]: true,
+        'pr-7': removable,
+        'border-primary-700 text-white bg-primary-700': selected,
+        'border-primary-200 text-primary-700 bg-primary-100': !selected,
+        'hover:bg-primary-500 hover:text-white cursor-pointer hover:border-transparent hover:ring hover:ring-primary-200': clickable
+    }
+    ">
         <slot></slot>
         <button v-if="removable" @click="remove" class="absolute h-full aspect-[1/1] top-0 right-0 bg-primary-100 border-l border-primary-200 hover:bg-red-300 hover:border-l-transparent hover:ring-1 hover:ring-red-500 hover:border-red-400 hover:text-red-600 rounded-r-full flex justify-center items-center text-xs">
             <Icon name="heroicons-solid:x-mark" />
